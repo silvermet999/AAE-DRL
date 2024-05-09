9c183f39d5b99ed5f794227aa81f64ebfcfe3d56
silvermet999 <ines.benmakhlouf00@gmail.com>
2024-05-09 16:35:39 -0400

    test

--- AAE.py
+++ AAE.py
@@ -1,1 +1,130 @@
 import main
+import argparse
+import os
+import numpy as np
+import math
+import itertools
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from torch import Tensor
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+parser.add_argument("--latent_dim", type=int, default=10, help="dimensionality of the latent code")
+parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
+parser.add_argument("--channels", type=int, default=1, help="number of image channels")
+parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+opt = parser.parse_args()
+print(opt)
+
+img_shape = (opt.channels, opt.img_size, opt.img_size)
+
+cuda = True if torch.cuda.is_available() else False
+
+
+def reparameterization(mu, logvar):
+    std = torch.exp(logvar / 2)
+    """ 
+    stockasticity :
+    samples sampled_z from a standard normal distribution (np.random.normal(0, 1, ...)) with the same shape as mu; 
+    the mean of the distribution in the latent space.
+    Instead of directly using the mean mu to represent the latent variable, the model samples from a distribution around mu.
+    
+    Backpropagation:
+    sampled_z seperate from mu and logvar.
+    """
+    sampled_z = Variable(Tensor(np.random.normal(0, 1, (mu.size(0), opt.latent_dim))))
+    z = sampled_z * std + mu
+    return z
+
+
+# module compatible with PyTorch
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(int(np.prod(img_shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        # projects output to the dim of latent space
+        self.mu = nn.Linear(512, opt.latent_dim)
+        self.logvar = nn.Linear(512, opt.latent_dim)
+
+# forward propagation
+    def forward(self, img):
+        # view() flatten
+        img_flat = img.view(img.shape[0], -1)
+        x = self.model(img_flat)
+        mu = self.mu(x)
+        logvar = self.logvar(x)
+        z = reparameterization(mu, logvar)
+        return z
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(opt.latent_dim, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, int(np.prod(img_shape))),
+            nn.Tanh(),
+        )
+
+    def forward(self, z):
+        img_flat = self.model(z)
+        img = img_flat.view(img_flat.shape[0], *img_shape)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(opt.latent_dim, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, z):
+        validity = self.model(z)
+        return validity
+
+
+# Use binary cross-entropy loss
+adversarial_loss = torch.nn.BCELoss()
+pixelwise_loss = torch.nn.L1Loss()
+
+# Initialize generator and discriminator
+encoder = Encoder()
+decoder = Decoder()
+discriminator = Discriminator()
+
+if cuda:
+    encoder.cuda()
+    decoder.cuda()
+    discriminator.cuda()
+    adversarial_loss.cuda()
+    pixelwise_loss.cuda()
