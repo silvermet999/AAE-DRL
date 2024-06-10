41e2704140328f12189fff515b605d59107dbe12
silvermet999 <ines.benmakhlouf00@gmail.com>
2024-06-10 12:52:31 -0400

    changed archi and train test split

--- AAE_pytorch.py
+++ AAE_pytorch.py
@@ -1,8 +1,8 @@
 """-----------------------------------------------import libraries-----------------------------------------------"""
 import main
 import argparse
-import os
 import numpy as np
+import pandas as pd
 import math
 import itertools
 import torchvision.transforms as transforms
@@ -11,9 +11,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 from torch import Tensor
+from torch.utils.data import DataLoader
 from torchsummary import summary
 from torch.nn import parallel as par
 from torch import distributed as dist
+from torch.optim import Adam
 
 
 
@@ -25,7 +27,7 @@ parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rat
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--latent_dim", type=int, default=10, help="dimensionality of the latent code")
+parser.add_argument("--z_dim", type=int, default=10, help="dimensionality of the latent code")
 parser.add_argument("--input_size", type=int, default=32, help="size of each input dimension")
 parser.add_argument("--channels", type=int, default=1, help="number of input channels")
 parser.add_argument("--sample_interval", type=int, default=400, help="interval between input sampling")
@@ -39,10 +41,12 @@ torch.cuda.memory_allocated()
 
 
 """-----------------------------------initialize variables for inputs and outputs-----------------------------------"""
-input_shape_rs = main.X_pca_rs.shape
-input_shape_mas = main.X_pca_mas.shape
+# input_shape_rs = main.X_pca_rs.shape
+# input_shape_mas = main.X_pca_mas.shape
+df_sel = main.df.iloc[:1000, :10]
+input_shape_rs = (50,10)
 nn_dim = 512
-latent_dim = 10
+z_dim = 10
 lr = 0.005
 
 
@@ -59,7 +63,7 @@ def reparameterization(mu, logvar):
     Backpropagation:
     sampled_z seperate from mu and logvar.
     """
-    sampled_z = Variable(Tensor(np.random.normal(0, 1, (mu.size(0), opt.latent_dim))))
+    sampled_z = Variable(Tensor(np.random.normal(0, 1, (mu.size(0), opt.z_dim))))
     z = sampled_z * std + mu
     return z
 
@@ -78,8 +82,8 @@ class EncoderGenerator(nn.Module):
         )
 
         # projects output to the dim of latent space
-        self.mu = nn.Linear(nn_dim, opt.latent_dim)
-        self.logvar = nn.Linear(nn_dim, opt.latent_dim)
+        self.mu = nn.Linear(nn_dim, opt.z_dim)
+        self.logvar = nn.Linear(nn_dim, opt.z_dim)
 
 
 # forward propagation
@@ -98,7 +102,7 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
 
         self.model = nn.Sequential(
-            nn.Linear(opt.latent_dim, nn_dim),
+            nn.Linear(opt.z_dim, nn_dim),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(512, 512),
             nn.BatchNorm1d(512),
@@ -118,7 +122,7 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
 
         self.model = nn.Sequential(
-            nn.Linear(opt.latent_dim, nn_dim),
+            nn.Linear(opt.z_dim, nn_dim),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Linear(512, 256),
             nn.LeakyReLU(0.2, inplace=True),
@@ -138,12 +142,85 @@ pixelwise_loss = torch.nn.L1Loss().cuda()
 
 
 encoder_generator = EncoderGenerator().cuda()
-# decoder = Decoder().cuda()
-# discriminator = Discriminator().cuda()
+decoder = Decoder().cuda()
+discriminator = Discriminator().cuda()
+
+
+optimizer_G = torch.optim.Adam(
+    itertools.chain(encoder_generator.parameters(), decoder.parameters()), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+Tensor = torch.cuda.FloatTensor
+
+def sample_runs(n_row, batches_done):
+    # Sample noise
+    z = Variable(Tensor(np.random.normal(0, 1, (n_row ** 2, opt.z_dim))))
+    gen_input = decoder(z)
+    gen_data = gen_input.data.cuda().numpy()
+    df = pd.DataFrame(gen_data)
+    df.to_csv(f"runs/{batches_done}.csv", index=False)
+
+# data_loader = DataLoader(df_sel, batch_size=6, shuffle=True)
+
+for epoch in range(opt.n_epochs):
+    for i, row in enumerate(df_sel.iterrows()):
+        j = row[1][df_sel.columns]
+        j = torch.tensor(j, dtype=torch.int).cuda().unsqueeze(0)
+
+        # Adversarial ground truths
+        valid = torch.ones((j.shape[0], 1))
+        fake = torch.zeros((j.shape[0], 1))
+
+        # Configure input
+        real = j.to(dtype=torch.float)
+
+        # -----------------
+        #  Train Generator
+        # -----------------
+
+        optimizer_G.zero_grad()
+
+        encoded = encoder_generator(real)
+        decoded = decoder(encoded)
+
+        # Loss measures generator's ability to fool the discriminator
+        g_loss = 0.001 * adversarial_loss(discriminator(encoded), valid) + 0.999 * pixelwise_loss(
+            decoded, real
+        )
+
+        g_loss.backward()
+        optimizer_G.step()
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+
+        optimizer_D.zero_grad()
+
+        # Sample noise as discriminator ground truth
+        z = Variable(Tensor(np.random.normal(0, 1, (j.shape[0], opt.z_dim))))
+
+        # Measure discriminator's ability to classify real from generated samples
+        real_loss = adversarial_loss(discriminator(z), valid)
+        fake_loss = adversarial_loss(discriminator(encoded.detach()), fake)
+        d_loss = 0.5 * (real_loss + fake_loss)
+
+        d_loss.backward()
+        optimizer_D.step()
+
+        print(
+            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+            % (epoch, opt.n_epochs, i, len(df_sel), d_loss.item(), g_loss.item())
+        )
+
+        batches_done = epoch * len(df_sel) + i
+        if batches_done % opt.sample_interval == 0:
+            sample_runs(n_row=5, batches_done=batches_done)
 
 
 
-"""_________________________________in case of more than one gpu (not the case here)_________________________________"""
+
+
+"""__________________________________________in case of more than one gpu__________________________________________"""
 def init_process():
     dist.init_process_group(
         backend='gloo',
