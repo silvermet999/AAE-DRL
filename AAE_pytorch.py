df440f817fe32d5894b61c6793a79d00d8cd3672
silvermet999 <ines.benmakhlouf00@gmail.com>
2024-06-14 04:46:56 -0400

    initial change of archi

--- AAE_pytorch.py
+++ AAE_pytorch.py
@@ -7,8 +7,9 @@ import math
 import itertools
 import torchvision.transforms as transforms
 import torch
-import torch.nn as nn
+from torch import nn
 from torch import Tensor, cuda, exp
+from torch.nn import functional as F
 from torchsummary import summary
 from torch.nn import parallel as par
 from torch import distributed as dist
@@ -39,9 +40,9 @@ cuda = True if cuda.is_available() else False
 """-----------------------------------initialize variables for inputs and outputs-----------------------------------"""
 # input_shape_rs = main.X_pca_rs.shape
 # input_shape_mas = main.X_pca_mas.shape
-df_sel = main.df.iloc[:500, :100]
-input_shape_rs = 100
-nn_dim = 500
+df_sel = main.df.iloc[:1000, :100]
+input_shape_rs = 1000
+nn_dim = 100
 z_dim = 10
 lr = 0.005
 
@@ -64,16 +65,31 @@ def reparameterization(mu, logvar):
     return z
 
 
+class Residual(nn.Module):
+    def __init__(self, i, o):
+        super(Residual, self).__init__()
+        self.fc = nn.Linear(i, o)
+        self.bn = nn.BatchNorm1d(o)
+        self.relu = nn.ReLU()
+
+    def forward(self, input):
+        out = self.fc(input)
+        out = self.bn(out)
+        out = self.relu(out)
+        return torch.cat([out, input], dim=1)
+
+
+
 # module compatible with PyTorch
 class EncoderGenerator(nn.Module):
     def __init__(self):
         super(EncoderGenerator, self).__init__()
 
         self.model = nn.Sequential(
-            nn.Linear(int(np.prod(input_shape_rs)), 500),
+            nn.Linear(input_shape_rs, 100),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(500, 500),
-            nn.BatchNorm1d(500),
+            nn.Linear(100, 100),
+            nn.BatchNorm1d(100),
             nn.LeakyReLU(0.2, inplace=True),
         )
 
@@ -84,8 +100,6 @@ class EncoderGenerator(nn.Module):
 
 # forward propagation
     def forward(self, input_flat):
-        # view() flatten
-        # input_flat = input.view(input.shape[0], -1)
         x = self.model(input_flat)
         mu = self.mu(x)
         logvar = self.logvar(x)
@@ -100,35 +114,187 @@ class Decoder(nn.Module):
         self.model = nn.Sequential(
             nn.Linear(opt.z_dim, nn_dim),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(500, 500),
-            nn.BatchNorm1d(500),
+            nn.Linear(100, 100),
+            nn.BatchNorm1d(100),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(500, int(np.prod(input_shape_rs))),
+            nn.Linear(100, input_shape_rs),
             nn.Tanh(),
         )
 
     def forward(self, z):
         input_flat = self.model(z)
-        # input = input_flat.view(input_flat.shape[0], *input_shape_rs)
         return input_flat
 
 
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(self, nn_dim, dim=10):
         super(Discriminator, self).__init__()
+        # dim =
 
-        self.model = nn.Sequential(
-            nn.Linear(opt.z_dim, nn_dim),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(500, 256),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(256, 1),
-            nn.Sigmoid(),
-        )
+        # self.model = nn.Sequential(
+        #     nn.Linear(opt.z_dim, nn_dim),
+        #     nn.LeakyReLU(0.2, inplace=True),
+        #     nn.Linear(100, 256),
+        #     nn.LeakyReLU(0.2, inplace=True),
+        #     nn.Linear(256, 1),
+        #     nn.Sigmoid(),
+        # )
+        seq = []
+        for i in list(nn_dim):
+            seq += [
+                nn.Linear(opt.z_dim, i),
+                nn.LeakyReLU(0.2, inplace=True),
+                nn.Dropout(0.5),
+                nn.Linear(100, 256),
+                nn.LeakyReLU(0.2, inplace=True),
+                nn.Linear(256, 1),
+                nn.Sigmoid()
+            ]
+            dim = i
+        seq += [nn.Linear(dim, 1)]
+        self.seq = nn.Sequential(*seq)
 
     def forward(self, z):
         validity = self.model(z)
         return validity
+
+
+
+
+def random_choice_prob_index(a, axis=1):
+    r = np.expand_dims(np.random.rand(a.shape[1 - axis]), axis=axis)
+    return (a.cumsum(axis=axis) > r).argmax(axis=axis)
+
+
+
+
+
+class Cond(object):
+    def __init__(self, data, output_info):
+        self.model = []
+
+        st = 0
+        skip = False
+        max_interval = 0
+        counter = 0
+        for item in output_info:
+            if item[1] == 'tanh':
+                st += item[0]
+                skip = True
+                continue
+            elif item[1] == 'softmax':
+                if skip:
+                    skip = False
+                    st += item[0]
+                    continue
+
+                ed = st + item[0]
+                max_interval = max(max_interval, ed - st)
+                counter += 1
+                self.model.append(np.argmax(data[:, st:ed], axis=-1))
+                st = ed
+            else:
+                assert 0
+        assert st == data.shape[1]
+
+        self.interval = []
+        self.n_col = 0
+        self.n_opt = 0
+        skip = False
+        st = 0
+        self.p = np.zeros((counter, max_interval))
+        for item in output_info:
+            if item[1] == 'tanh':
+                skip = True
+                st += item[0]
+                continue
+            elif item[1] == 'softmax':
+                if skip:
+                    st += item[0]
+                    skip = False
+                    continue
+                ed = st + item[0]
+                tmp = np.sum(data[:, st:ed], axis=0)
+                tmp = np.log(tmp + 1)
+                tmp = tmp / np.sum(tmp)
+                self.p[self.n_col, :item[0]] = tmp
+                self.interval.append((self.n_opt, item[0]))
+                self.n_opt += item[0]
+                self.n_col += 1
+                st = ed
+            else:
+                assert 0
+        self.interval = np.asarray(self.interval)
+
+    def sample(self, batch):
+        if self.n_col == 0:
+            return None
+        batch = batch
+        idx = np.random.choice(np.arange(self.n_col), batch)
+
+        vec1 = np.zeros((batch, self.n_opt), dtype='float32')
+        mask1 = np.zeros((batch, self.n_col), dtype='float32')
+        mask1[np.arange(batch), idx] = 1
+        opt1prime = random_choice_prob_index(self.p[idx])
+        opt1 = self.interval[idx, 0] + opt1prime
+        vec1[np.arange(batch), opt1] = 1
+
+        return vec1, mask1, idx, opt1prime
+
+    def sample_zero(self, batch):
+        if self.n_col == 0:
+            return None
+        vec = np.zeros((batch, self.n_opt), dtype='float32')
+        idx = np.random.choice(np.arange(self.n_col), batch)
+        for i in range(batch):
+            col = idx[i]
+            pick = int(np.random.choice(self.model[col]))
+            vec[i, pick + self.interval[col, 0]] = 1
+        return vec
+
+
+
+
+
+def cond_loss(data, output_info, c, m):
+    loss = []
+    st = 0
+    st_c = 0
+    skip = False
+    for item in output_info:
+        if item[1] == 'tanh':
+            st += item[0]
+            skip = True
+
+        elif item[1] == 'softmax':
+            if skip:
+                skip = False
+                st += item[0]
+                continue
+
+            ed = st + item[0]
+            ed_c = st_c + item[0]
+            tmp = F.binary_cross_entropy(
+                data[:, st:ed],
+                torch.argmax(c[:, st_c:ed_c], dim=1),
+                reduction='none'
+            )
+            loss.append(tmp)
+            st = ed
+            st_c = ed_c
+
+        else:
+            assert 0
+    loss = torch.stack(loss, dim=1)
+
+    return (loss * m).sum() / data.size()[0]
+
+
+
+
+
+
+
 
 
 # Use binary cross-entropy loss
@@ -194,7 +360,7 @@ for epoch in range(opt.n_epochs):
     optimizer_D.step()
 
     print(
-        epoch, opt.n_epochs, len(df_sel), d_loss.item(), g_loss.item()
+        epoch, opt.n_epochs, d_loss.item(), g_loss.item()
     )
 
     batches_done = epoch * len(df_sel)
@@ -204,4 +370,217 @@ for epoch in range(opt.n_epochs):
 
 
 
+
+
+
+
+
+class Sampler(object):
+    """docstring for Sampler."""
+
+    def __init__(self, data, output_info):
+        super(Sampler, self).__init__()
+        self.data = data
+        self.model = []
+        self.n = len(data)
+
+        st = 0
+        skip = False
+        for item in output_info:
+            if item[1] == 'tanh':
+                st += item[0]
+                skip = True
+            elif item[1] == 'softmax':
+                if skip:
+                    skip = False
+                    st += item[0]
+                    continue
+                ed = st + item[0]
+                tmp = []
+                for j in range(item[0]):
+                    tmp.append(np.nonzero(data[:, st + j])[0])
+                self.model.append(tmp)
+                st = ed
+            else:
+                assert 0
+        assert st == data.shape[1]
+
+    def sample(self, n, col, opt):
+        if col is None:
+            idx = np.random.choice(np.arange(self.n), n)
+            return self.data[idx]
+        idx = []
+        for c, o in zip(col, opt):
+            idx.append(np.random.choice(self.model[c][o]))
+        return self.data[idx]
+
+
+def calc_gradient_penalty(netD, real_data, fake_data, device='cpu', pac=10, lambda_=10):
+    alpha = torch.rand(real_data.size(0) // pac, 1, 1, device=device)
+    alpha = alpha.repeat(1, pac, real_data.size(1))
+    alpha = alpha.view(-1, real_data.size(1))
+
+    interpolates = alpha * real_data + ((1 - alpha) * fake_data)
+
+    disc_interpolates = netD(interpolates)
+
+    gradients = torch.autograd.grad(
+        outputs=disc_interpolates, inputs=interpolates,
+        grad_outputs=torch.ones(disc_interpolates.size(), device=device),
+        create_graph=True, retain_graph=True, only_inputs=True)[0]
+
+    gradient_penalty = (
+        (gradients.view(-1, pac * real_data.size(1)).norm(2, dim=1) - 1) ** 2).mean() * lambda_
+    return gradient_penalty
+
+
+class CTGANSynthesizer(BaseSynthesizer):
+    def __init__(self,
+                 dataset_name,
+                 args=None):
+
+        self.dataset_name = dataset_name
+        self.embedding_dim = args.embedding_dim
+        self.gen_dim = args.gen_dim
+        self.dis_dim = args.dis_dim
+        self.lr = args.lr
+
+        self.l2scale = args.l2scale
+        self.batch_size = args.batch_size
+        self.epochs = args.epochs
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.scores_max = 0
+
+    def fit(self, train_data, categorical_columns=tuple(), ordinal_columns=tuple()):
+
+        self.transformer = BGMTransformer()
+        self.transformer.fit(train_data, categorical_columns, ordinal_columns)
+        train_data = self.transformer.transform(train_data)
+
+        data_sampler = Sampler(train_data, self.transformer.output_info)
+        data_dim = self.transformer.output_dim
+        self.cond_generator = Cond(train_data, self.transformer.output_info)
+
+        self.generator = Generator(
+            self.embedding_dim + self.cond_generator.n_opt,
+            self.gen_dim,
+            data_dim).to(self.device)
+
+        discriminator = Discriminator(
+            data_dim + self.cond_generator.n_opt,
+            self.dis_dim).to(self.device)
+
+        optimizerG = optim.Adam(
+            self.generator.parameters(), lr=self.lr, betas=(0.5, 0.9), weight_decay=self.l2scale)
+        optimizerD = optim.Adam(discriminator.parameters(), lr=self.lr, betas=(0.5, 0.9))
+
+        if len(train_data) <= self.batch_size:
+            self.batch_size = (len(train_data) // 10) * 10
+
+        assert self.batch_size % 2 == 0
+        mean = torch.zeros(self.batch_size, self.embedding_dim, device=self.device)
+        std = mean + 1
+
+        steps_per_epoch = len(train_data) // self.batch_size
+
+        for i in range(self.epochs):
+            print(i)
+            for id_ in range(steps_per_epoch):
+                fakez = torch.normal(mean=mean, std=std)
+
+                condvec = self.cond_generator.sample(self.batch_size)
+                if condvec is None:
+                    c1, m1, col, opt = None, None, None, None
+                    real = data_sampler.sample(self.batch_size, col, opt)
+                else:
+                    c1, m1, col, opt = condvec
+                    c1 = torch.from_numpy(c1).to(self.device)
+                    m1 = torch.from_numpy(m1).to(self.device)
+                    fakez = torch.cat([fakez, c1], dim=1)
+
+                    perm = np.arange(self.batch_size)
+                    np.random.shuffle(perm)
+                    real = data_sampler.sample(self.batch_size, col[perm], opt[perm])
+                    c2 = c1[perm]
+
+                fake = self.generator(fakez)
+                fakeact = apply_activate(fake, self.transformer.output_info)
+
+                real = torch.from_numpy(real.astype('float32')).to(self.device)
+
+                if c1 is not None:
+                    fake_cat = torch.cat([fakeact, c1], dim=1)
+                    real_cat = torch.cat([real, c2], dim=1)
+                else:
+                    real_cat = real
+                    fake_cat = fake
+
+                y_fake = discriminator(fake_cat)
+                y_real = discriminator(real_cat)
+
+                loss_d = -torch.mean(y_real) + torch.mean(y_fake)
+                pen = calc_gradient_penalty(discriminator, real_cat, fake_cat, self.device)
+
+                optimizerD.zero_grad()
+                pen.backward(retain_graph=True)
+                loss_d.backward()
+                optimizerD.step()
+
+                fakez = torch.normal(mean=mean, std=std)
+                condvec = self.cond_generator.sample(self.batch_size)
+
+                if condvec is None:
+                    c1, m1, col, opt = None, None, None, None
+                else:
+                    c1, m1, col, opt = condvec
+                    c1 = torch.from_numpy(c1).to(self.device)
+                    m1 = torch.from_numpy(m1).to(self.device)
+                    fakez = torch.cat([fakez, c1], dim=1)
+
+                fake = self.generator(fakez)
+                fakeact = apply_activate(fake, self.transformer.output_info)
+
+                if c1 is not None:
+                    y_fake = discriminator(torch.cat([fakeact, c1], dim=1))
+                else:
+                    y_fake = discriminator(fakeact)
+
+                if condvec is None:
+                    cross_entropy = 0
+                else:
+                    cross_entropy = cond_loss(fake, self.transformer.output_info, c1, m1)
+
+                loss_g = -torch.mean(y_fake) + cross_entropy
+
+                optimizerG.zero_grad()
+                loss_g.backward()
+                optimizerG.step()
+
+    def sample(self, n):
+
+        self.generator.eval()
+
+        output_info = self.transformer.output_info
+        steps = n // self.batch_size + 1
+        data = []
+        for i in range(steps):
+            mean = torch.zeros(self.batch_size, self.embedding_dim)
+            std = mean + 1
+            fakez = torch.normal(mean=mean, std=std).to(self.device)
+
+            condvec = self.cond_generator.sample_zero(self.batch_size)
+            if condvec is None:
+                pass
+            else:
+                c1 = condvec
+                c1 = torch.from_numpy(c1).to(self.device)
+                fakez = torch.cat([fakez, c1], dim=1)
+
+            fake = self.generator(fakez)
+            fakeact = apply_activate(fake, output_info)
+            data.append(fakeact.detach().cpu().numpy())
+
+        data = np.concatenate(data, axis=0)
+        data = data[:n]
+        return self.transformer.inverse_transform(data, None)
 
