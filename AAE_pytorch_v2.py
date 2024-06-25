45b6d28e8dea36ed7fac44954152a54c547d61ba
silvermet999 <ines.benmakhlouf00@gmail.com>
2024-06-25 15:04:46 -0400

    mlflow

--- AAE_pytorch_v2.py
+++ AAE_pytorch_v2.py
@@ -1,14 +1,14 @@
 """-----------------------------------------------import libraries-----------------------------------------------"""
 import main
 import clf
-import dim_reduction
-from sklearn.tree import DecisionTreeClassifier
 from synthetic_data import SyntheticData
 import argparse
 import numpy as np
 import pandas as pd
 import itertools
-import torch
+from torch import cat, rand, autograd, ones, FloatTensor, tensor, zeros, float
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ExponentialLR, MultiStepLR
 from torch.nn import BatchNorm1d, LeakyReLU, Linear, Module, Sequential, Tanh, Sigmoid, BCELoss, L1Loss
 from torch import Tensor, cuda, exp
 from torchsummary import summary
@@ -17,19 +17,6 @@ import mlflow
 
 
 """-----------------------------------------------command-line options-----------------------------------------------"""
-parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=10, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=12, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.001, help="adam: learning rate")
-parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--z_dim", type=int, default=10, help="dimensionality of the latent code")
-parser.add_argument("--input_size", type=int, default=32, help="size of each input dimension")
-parser.add_argument("--channels", type=int, default=1, help="number of input channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval between input sampling")
-opt = parser.parse_args()
-print(opt)
 cuda = True if cuda.is_available() else False
 
 # torch_gpu = torch.empty((20000, 20000)).cuda()
@@ -45,11 +32,17 @@ hl_dim = (100, 100, 100, 100, 100)
 hl_dimd = (10, 10, 10, 10, 10)
 out_in_dim = 100 # in for the dec and disc out for the enc/gen
 z_dim = 10
-lr = 0.005
+params = {
+    "lr": 0.01,
+    "batch_size": 24,
+    "n_epochs": 100,
+    "optimizer": "Adam",
+    "gamma": 0.9
+}
 
 
 
-"""-----------------------------------------------------classes-----------------------------------------------------"""
+"""--------------------------------------------backprop and hidden layers--------------------------------------------"""
 def reparameterization(mu, logvar, z_dim):
     std = exp(logvar / 2)
     """ 
@@ -66,6 +59,7 @@ def reparameterization(mu, logvar, z_dim):
     return z
 
 
+#loop for hidden layers
 class Residual(Module):
     def __init__(self, i, o):
         super(Residual, self).__init__()
@@ -77,9 +71,11 @@ class Residual(Module):
         l1 = self.fc1(l0)
         l2 = self.leakyrelu1(l1)
         l3= self.bn(l2)
-        return torch.cat([l3, l0], dim=1)
+        return cat([l3, l0], dim=1)
 
 
+
+"""----------------------------------------------------AAE blocks----------------------------------------------------"""
 # module compatible with PyTorch
 class EncoderGenerator(Module):
     def __init__(self):
@@ -96,7 +92,6 @@ class EncoderGenerator(Module):
         # projects output to the dim of latent space
         self.mu = Linear(out_in_dim, z_dim)
         self.logvar = Linear(out_in_dim, z_dim)
-
 
 # forward propagation
     def forward(self, input_):
@@ -123,7 +118,6 @@ class Decoder(Module):
         return input_
 
 
-
 class Discriminator(Module):
     def __init__(self, pack=10):
         super(Discriminator, self).__init__()
@@ -142,14 +136,12 @@ class Discriminator(Module):
         seq += [Linear(dim, 1), Sigmoid()]
         self.seq = Sequential(*seq)
 
-
     def forward(self, input_):
         return self.seq(input_)
 
 
 
-
-# Use binary cross-entropy loss
+"""--------------------------------------------------loss and optim--------------------------------------------------"""
 adversarial_loss = BCELoss().cuda() if cuda else BCELoss()
 recon_loss = L1Loss().cuda() if cuda else L1Loss()
 
@@ -164,7 +156,7 @@ summary(discriminator, input_size=(z_dim,))
 
 
 def calc_gradient_penalty(netD, real_data, fake_data, device='cpu', pac=10, lambda_=10):
-    alpha = torch.rand(real_data.size(0) // pac, 1, 1, device=device)
+    alpha = rand(real_data.size(0) // pac, 1, 1, device=device)
     alpha = alpha.repeat(1, pac, real_data.size(1))
     alpha = alpha.view(-1, real_data.size(1))
 
@@ -172,9 +164,9 @@ def calc_gradient_penalty(netD, real_data, fake_data, device='cpu', pac=10, lamb
 
     disc_interpolates = netD(interpolates)
 
-    gradients = torch.autograd.grad(
+    gradients = autograd.grad(
         outputs=disc_interpolates, inputs=interpolates,
-        grad_outputs=torch.ones(disc_interpolates.size(), device=device),
+        grad_outputs=ones(disc_interpolates.size(), device=device),
         create_graph=True, retain_graph=True, only_inputs=True)[0]
 
     gradient_penalty = (
@@ -182,12 +174,20 @@ def calc_gradient_penalty(netD, real_data, fake_data, device='cpu', pac=10, lamb
     return gradient_penalty
 
 
+mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
+mlflow.set_experiment("test_experiment")
+mlflow.log_param(params)
 
-optimizer_G = torch.optim.Adam(
-    itertools.chain(encoder_generator.parameters(), decoder.parameters()), lr=opt.lr, betas=(opt.b1, opt.b2))
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
+optimizer_G = Adam(
+    itertools.chain(encoder_generator.parameters(), decoder.parameters()), lr=0.01, betas=(0.5, 0.99))
+optimizer_D = Adam(discriminator.parameters(), lr=0.01, betas=(0.01, 0.9))
+scheduler1 = MultiStepLR(optimizer_G, milestones=[30,80], gamma=0.1)
+scheduler2 = ExponentialLR(optimizer_D, gamma=0.9)
+
+
+
+"""-----------------------------------------------------data gen-----------------------------------------------------"""
 def sample_runs(n_row, z_dim, batches_done):
     # Sample noise
     z = Tensor(np.random.lognormal(0, 1, (n_row ** 2, z_dim)))
@@ -198,53 +198,50 @@ def sample_runs(n_row, z_dim, batches_done):
 
 
 
-
-for epoch in range(1):
+"""--------------------------------------------------model training--------------------------------------------------"""
+for epoch in range(100):
     n_batch = len(df_sel) // 24
     for i in range(n_batch):
         str_idx = i * 24
         end_idx = str_idx + 24
         batch_data = df_sel.iloc[str_idx:end_idx]
-        batch_tensor = torch.tensor(batch_data.values, dtype=torch.float).cuda() if cuda else torch.tensor(
-            batch_data.values, dtype=torch.float)
+        batch_tensor = tensor(batch_data.values, dtype=float).cuda() if cuda else tensor(
+            batch_data.values, dtype=float)
 
         real = (batch_tensor - batch_tensor.mean()) / batch_tensor.std()
-        valid = torch.ones((batch_tensor.shape[0], 1)).cuda() if cuda else torch.ones((batch_tensor.shape[0], 1))
-        fake = torch.zeros((batch_tensor.shape[0], 1)).cuda() if cuda else torch.zeros((batch_tensor.shape[0], 1))
+        valid = ones((batch_tensor.shape[0], 1)).cuda() if cuda else ones((batch_tensor.shape[0], 1))
+        fake = zeros((batch_tensor.shape[0], 1)).cuda() if cuda else zeros((batch_tensor.shape[0], 1))
         optimizer_G.zero_grad()
 
         encoded = encoder_generator(real)
         decoded = decoder(encoded)
+
         g_loss = 0.001 * adversarial_loss(discriminator(encoded), valid) + 0.999 * recon_loss(
                     decoded, real
                 )
-
         g_loss.backward()
         optimizer_G.step()
 
         optimizer_D.zero_grad()
-
-        # Sample noise as discriminator ground truth
         z = Tensor(np.random.lognormal(0, 1, (batch_tensor.shape[0], z_dim)))
-
         # real and fake loss should be close
         # discriminator(z) should be close to 0
         real_loss = adversarial_loss(discriminator(z), valid)
         fake_loss = adversarial_loss(discriminator(encoded.detach()), fake)
         d_loss = 0.5 * (real_loss + fake_loss)
-
         d_loss.backward()
         optimizer_D.step()
 
+        scheduler1.step()
+        scheduler2.step()
 
-
-        print(epoch, opt.n_epochs, d_loss.item(), g_loss.item())
-
+        print(epoch, n_batch, d_loss.item(), g_loss.item())
         batches_done = epoch * len(df_sel)
-        if batches_done % opt.sample_interval == 0:
+        if batches_done % 400 == 0:
             sample_runs(n_row=71, z_dim=10, batches_done=3)
 
-    # print(clf.classifier())
+
+    print(clf.xgb())
 
 
 # mlflow server --host 127.0.0.1 --port 8080
