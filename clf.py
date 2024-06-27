6f016f6070d0207deb9954f972b059913f86c607
silvermet999 <ines.benmakhlouf00@gmail.com>
2024-06-26 20:26:39 -0400

    fix xgb

--- clf.py
+++ clf.py
@@ -3,13 +3,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import tree
 from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, IsolationForest
+from sklearn.mixture import GaussianMixture
+from sklearn.model_selection import StratifiedKFold
+from sklearn.pipeline import make_pipeline
+from imblearn.pipeline import Pipeline
+
 from sklearn.svm import SVC
-from xgboost import XGBClassifier
+import xgboost
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import LabelEncoder
+from imblearn.over_sampling import SMOTE
+
 import dim_reduction
 import main
 from scipy.stats import mode
+from skopt import BayesSearchCV, space
 
 
 
@@ -58,10 +66,57 @@ y_train = le.fit_transform(main.y_train["Category"])
 y_test = le.fit_transform(main.y_test["Category"])
 
 def xgb():
-    clf = XGBClassifier(n_estimators=2, max_depth=2, learning_rate=1, objective='multi:softmax', n_class = 14)
+    clf = xgboost.XGBClassifier(n_estimators=14, max_depth=5, learning_rate=0.1, objective='multi:softmax', n_class = 14)
     clf.fit(main.x_train, y_train)
     pred = clf.predict(main.x_test)
-    class_report = classification_report(y_test[:10688], pred)
+    label_mapping = {i: label for i, label in enumerate(np.unique(y_train))}
+    class_report = classification_report(y_test[:10688], pred, target_names=[label_mapping[i] for i in np.unique(y_train)])
     with open('xgb_classification_report.txt', 'w') as f:
         f.write(class_report)
     return pred, class_report
+
+
+sm = SMOTE(random_state=42)
+X_res, y_res = sm.fit_resample(main.x_train, y_train)
+
+dtrain = xgboost.DMatrix(X_res, label=y_res)
+dtest = xgboost.DMatrix(main.x_test, label=y_test)
+
+
+
+def xgb_bscv():
+    smote = SMOTE(random_state=42)
+    clf = xgboost.XGBClassifier(n_estimators=14, max_depth=5, gamma=0.1, reg_alpha=1.0,
+                            reg_lambda=1.0, learning_rate=0.1, objective='multi:softmax',
+                            n_class=14)
+
+    opt = BayesSearchCV(
+        clf,
+        search_spaces={
+            'n_estimators': space.Integer(10, 100),
+            'max_depth': space.Integer(5, 10),
+            'learning_rate': space.Real(0.01, 1.0, 'log-uniform')
+        },
+        n_iter=10,
+        random_state=0,
+        verbose=1,
+        n_jobs=-1,
+        scoring='accuracy',
+        cv=3
+    )
+
+    pipeline = Pipeline([('smote', smote), ('opt', opt)])
+
+    pipeline.fit(main.x_train, y_train)
+    pred = pipeline.predict(main.x_test)
+    class_report = classification_report(y_test[:10688], pred)
+
+    with open('xgb_bscv_smote_classification_report.txt', 'w') as f:
+        f.write(class_report)
+
+    return pred, class_report
+
+
+
+
+
