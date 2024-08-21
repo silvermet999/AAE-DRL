42b4b4fdb734333bfe170b2a047c5a2c4998c9a5
silver <ines.benmakhlouf00@gmail.com>
2024-08-21 15:50:57 +0100

    archi

--- AAE2/AAE_archi.py
+++ AAE2/AAE_archi.py
@@ -21,7 +21,8 @@ os.environ['TORCH_USE_CUDA_DSA'] = "1"
 """---------------------------------------------backprop and hidden layers-------------------------------------------"""
 def reparameterization(mu, logvar, z_dim):
     std = exp(logvar / 2)
-    sampled_z = normal(0, 1, (mu.size(0), z_dim)).to("cuda")
+    device = mu.device
+    sampled_z = normal(0, 1, (mu.size(0), z_dim)).to(device)
     z = sampled_z * std + mu
     return z
 
@@ -29,145 +30,28 @@ def reparameterization(mu, logvar, z_dim):
 
 """----------------------------------------------------AAE blocks----------------------------------------------------"""
 params = {
-    "n_layers_e": 44,
-    "n_layers_de": 37,
-    "n_layers_di": 11,
-    "n_units_e_l0": 369,
-    "dropout_l0": 0.2912014074950425,
-    "n_units_e_l1": 264,
-    "dropout_l1": 0.35957829905370775,
-    "n_units_e_l2": 143,
-    "dropout_l2": 0.16634929998161474,
-    "n_units_e_l3": 418,
-    "dropout_l3": 0.14488264346896107,
-    "n_units_e_l4": 483,
-    "dropout_l4": 0.34961088800051693,
-    "n_units_e_l5": 340,
-    "dropout_l5": 0.2882157846404448,
-    "n_units_e_l6": 374,
-    "dropout_l6": 0.02374574253753159,
-    "n_units_e_l7": 185,
-    "dropout_l7": 0.2984299208956127,
-    "n_units_e_l8": 152,
-    "dropout_l8": 0.07317829255139408,
-    "n_units_e_l9": 225,
-    "dropout_l9": 0.4633390011826975,
-    "n_units_e_l10": 248,
-    "dropout_l10": 0.18925529370010746,
-    "n_units_e_l11": 406,
-    "dropout_l11": 0.04124974957292793,
-    "n_units_e_l12": 501,
-    "dropout_l12": 0.3050765260201068,
-    "n_units_e_l13": 252,
-    "dropout_l13": 0.4529985369856203,
-    "n_units_e_l14": 392,
-    "dropout_l14": 0.09256666951563924,
-    "n_units_e_l15": 313,
-    "dropout_l15": 0.0034110266440671166,
-    "n_units_e_l16": 166,
-    "dropout_l16": 0.2433992663000255,
-    "n_units_e_l17": 54,
-    "dropout_l17": 0.3637684362506816,
-    "n_units_e_l18": 142,
-    "dropout_l18": 0.11901943868375703,
-    "n_units_e_l19": 224,
-    "dropout_l19": 0.12485899981978027,
-    "n_units_e_l20": 321,
-    "dropout_l20": 0.40862458111926414,
-    "n_units_e_l21": 493,
-    "dropout_l21": 0.25096111389024267,
-    "n_units_e_l22": 303,
-    "dropout_l22": 0.17344412654847136,
-    "n_units_e_l23": 153,
-    "dropout_l23": 0.19765134545072033,
-    "n_units_e_l24": 138,
-    "dropout_l24": 0.0293149257260214,
-    "n_units_e_l25": 252,
-    "dropout_l25": 0.43794389274878254,
-    "n_units_e_l26": 110,
-    "dropout_l26": 0.4365971670818343,
-    "n_units_e_l27": 376,
-    "dropout_l27": 0.4932216553741478,
-    "n_units_e_l28": 245,
-    "dropout_l28": 0.17462278881475202,
-    "n_units_e_l29": 349,
-    "dropout_l29": 0.04790680468013431,
-    "n_units_e_l30": 478,
-    "dropout_l30": 0.189794748643973,
-    "n_units_e_l31": 424,
-    "dropout_l31": 0.28708515210653035,
-    "n_units_e_l32": 63,
-    "dropout_l32": 0.12792539876826858,
-    "n_units_e_l33": 204,
-    "dropout_l33": 0.04126183161713565,
-    "n_units_e_l34": 438,
-    "dropout_l34": 0.46693733936443116,
-    "n_units_e_l35": 390,
-    "dropout_l35": 0.16244944823482776,
-    "n_units_e_l36": 456,
-    "dropout_l36": 0.3438666329016294,
-    "n_units_e_l37": 294,
-    "dropout_l37": 0.3571553789563026,
-    "n_units_e_l38": 348,
-    "dropout_l38": 0.30372651918722593,
-    "n_units_e_l39": 239,
-    "dropout_l39": 0.3367211118052325,
-    "n_units_e_l40": 479,
-    "dropout_l40": 0.44870344468741424,
-    "n_units_e_l41": 260,
-    "dropout_l41": 0.00375152328986611,
-    "n_units_e_l42": 457,
-    "dropout_l42": 0.34571830645315565,
-    "n_units_e_l43": 310,
-    "dropout_l43": 0.3001775340913507,
-    "n_units_de_l0": 225,
-    "n_units_de_l1": 281,
-    "n_units_de_l2": 303,
-    "n_units_de_l3": 444,
-    "n_units_de_l4": 59,
-    "n_units_de_l5": 124,
-    "n_units_de_l6": 245,
-    "n_units_de_l7": 293,
-    "n_units_de_l8": 442,
-    "n_units_de_l9": 444,
-    "n_units_de_l10": 494,
-    "n_units_de_l11": 438,
-    "n_units_de_l12": 414,
-    "n_units_de_l13": 83,
-    "n_units_de_l14": 255,
-    "n_units_de_l15": 420,
-    "n_units_de_l16": 315,
-    "n_units_de_l17": 473,
-    "n_units_de_l18": 52,
-    "n_units_de_l19": 161,
-    "n_units_de_l20": 501,
-    "n_units_de_l21": 88,
-    "n_units_de_l22": 439,
-    "n_units_de_l23": 441,
-    "n_units_de_l24": 257,
-    "n_units_de_l25": 313,
-    "n_units_de_l26": 472,
-    "n_units_de_l27": 326,
-    "n_units_de_l28": 382,
-    "n_units_de_l29": 187,
-    "n_units_de_l30": 188,
-    "n_units_de_l31": 248,
-    "n_units_de_l32": 98,
-    "n_units_de_l33": 158,
-    "n_units_de_l34": 35,
-    "n_units_de_l35": 236,
-    "n_units_de_l36": 233,
-    "n_units_di_l0": 493,
-    "n_units_di_l1": 445,
-    "n_units_di_l2": 64,
-    "n_units_di_l3": 290,
-    "n_units_di_l4": 359,
-    "n_units_di_l5": 421,
-    "n_units_di_l6": 403,
-    "n_units_di_l7": 118,
-    "n_units_di_l8": 162,
-    "n_units_di_l9": 134,
-    "n_units_di_l10": 466,
+    'n_layers_e': 18, 'n_layers_de': 23, 'n_layers_di': 25, 'n_units_e_l0': 328, 'dropout_l0': 0.4508145404589306,
+    'n_units_e_l1': 132, 'dropout_l1': 0.462657336072197, 'n_units_e_l2': 36, 'dropout_l2': 0.4811891655137515,
+    'n_units_e_l3': 351, 'dropout_l3': 0.1325958233139718, 'n_units_e_l4': 83, 'dropout_l4': 0.2890545145126751,
+    'n_units_e_l5': 325, 'dropout_l5': 0.31814229345131784, 'n_units_e_l6': 95, 'dropout_l6': 0.35264665462044575,
+    'n_units_e_l7': 375, 'dropout_l7': 0.42977846340141135, 'n_units_e_l8': 124, 'dropout_l8': 0.13964236762852644,
+    'n_units_e_l9': 467, 'dropout_l9': 0.4252003827871995, 'n_units_e_l10': 274, 'dropout_l10': 0.37891589475027376,
+    'n_units_e_l11': 142, 'dropout_l11': 0.3732006358029489, 'n_units_e_l12': 434, 'dropout_l12': 0.4274768055805755,
+    'n_units_e_l13': 36, 'dropout_l13': 0.35029290179513484, 'n_units_e_l14': 477, 'dropout_l14': 0.1279745833821307,
+    'n_units_e_l15': 158, 'dropout_l15': 0.4378844177229787, 'n_units_e_l16': 270, 'dropout_l16': 0.4107626190212738,
+    'n_units_e_l17': 379, 'dropout_l17': 0.36862218469927654, 'n_units_de_l0': 196, 'n_units_de_l1': 259,
+    'n_units_de_l2': 53, 'n_units_de_l3': 35, 'n_units_de_l4': 256, 'n_units_de_l5': 113, 'n_units_de_l6': 440,
+    'n_units_de_l7': 328, 'n_units_de_l8': 457, 'n_units_de_l9': 428, 'n_units_de_l10': 200, 'n_units_de_l11': 86,
+    'n_units_de_l12': 138, 'n_units_de_l13': 317, 'n_units_de_l14': 183, 'n_units_de_l15': 128, 'n_units_de_l16': 320,
+    'n_units_de_l17': 278, 'n_units_de_l18': 465, 'dropout_l18': 0.12847305916739454, 'n_units_de_l19': 46,
+    'dropout_l19': 0.3909283296720884, 'n_units_de_l20': 491, 'dropout_l20': 0.34369909780464275, 'n_units_de_l21': 153,
+    'dropout_l21': 0.05718230695211901, 'n_units_de_l22': 450, 'dropout_l22': 0.3130787165053842, 'n_units_di_l0': 156,
+    'n_units_di_l1': 258, 'n_units_di_l2': 404, 'n_units_di_l3': 176, 'n_units_di_l4': 418, 'n_units_di_l5': 360,
+    'n_units_di_l6': 115, 'n_units_di_l7': 317, 'n_units_di_l8': 37, 'n_units_di_l9': 375, 'n_units_di_l10': 331,
+    'n_units_di_l11': 255, 'n_units_di_l12': 125, 'n_units_di_l13': 95, 'n_units_di_l14': 82, 'n_units_di_l15': 205,
+    'n_units_di_l16': 111, 'n_units_di_l17': 462, 'n_units_di_l18': 366, 'n_units_di_l19': 43, 'n_units_di_l20': 189,
+    'n_units_di_l21': 318, 'n_units_di_l22': 203, 'n_units_di_l23': 169, 'dropout_l23': 0.1396154156601358,
+    'n_units_di_l24': 76, 'dropout_l24': 0.3336433169832094
 
 }
 class EncoderGenerator(Module):
@@ -180,12 +64,13 @@ class EncoderGenerator(Module):
             seq.append(Linear(in_features, out_features))
             seq.append(LeakyReLU())
             seq.append(BatchNorm1d(out_features))
-            dropout = params[f'dropout_l{i}']
-            seq.append(Dropout(dropout))
+            if i <= 17:
+                dropout = params[f'dropout_l{i}']
+                seq.append(Dropout(dropout))
 
         self.seq = Sequential(*seq)
-        self.mu = Linear(310, 32)
-        self.logvar = Linear(310, 32)
+        self.mu = Linear(379, 32)
+        self.logvar = Linear(379, 32)
 
 
     def forward(self, x):
@@ -206,15 +91,16 @@ class Decoder(Module):
             seq.append(Linear(in_features, out_features))
             seq.append(LeakyReLU())
             seq.append(BatchNorm1d(out_features))
-        seq += [
-            Linear(out_features, in_out),
-            Tanh()
-        ]
+            if 18 <= i <= 22:
+                dropout = params[f'dropout_l{i}']
+                seq.append(Dropout(dropout))
+        seq.append(Linear(out_features, in_out))
+        seq.append(Tanh())
         self.seq = Sequential(*seq)
 
     def forward(self, z):
-        input_ = self.seq(z)
-        return input_
+        input_z = self.seq(z)
+        return input_z
 
 
 
@@ -229,19 +115,22 @@ class Discriminator(Module):
             seq.append(Linear(in_features, out_features))
             seq.append(LeakyReLU())
             seq.append(BatchNorm1d(out_features))
+            if i <= 23:
+                dropout = params[f'dropout_l{i}']
+                seq.append(Dropout(dropout))
 
         seq.append(Linear(out_features, 1))
         seq.append(Sigmoid())
         self.seq = Sequential(*seq)
 
 
-    def forward(self, input_):
-        return self.seq(input_)
+    def forward(self, input_z):
+        return self.seq(input_z)
 
 
-encoder_generator = EncoderGenerator().to("cuda:0")
-decoder = Decoder().to("cuda:1")
-discriminator = Discriminator().to("cuda:0")
+encoder_generator = EncoderGenerator()
+decoder = Decoder()
+discriminator = Discriminator()
 
 
 # with open('/home/silver/PycharmProjects/AAEDRL/AAE/hyperparams_g.json', 'r') as f:
