4a3a23931b13515c14269fb2a791d6fa00fee3ad
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2024-10-16 02:20:03 +0100

    interpolation

--- AAE_training.py
+++ AAE_training.py
@@ -12,13 +12,8 @@ from torch.nn import BCELoss, L1Loss
 from torch import cuda
 from data import main
 import itertools
-os.environ['CUDA_LAUNCH_BLOCKING']="1"
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 os.environ['TORCH_USE_CUDA_DSA'] = "1"
-
-
-# (100, 150, 100, 150, 100) 3159 002 10.5830 0848
-# (100, 200, 150, 100) 3149 002 3.9996 0.0140
-# (100, 150, 150, 150, 100) 3363 ± 0.0021 10.3411 ± 0.0249
 
 cuda = True if cuda.is_available() else False
 torch.cuda.empty_cache()
@@ -28,9 +23,6 @@ df_train = main.X_train_rs
 
 
 """--------------------------------------------------loss and optim--------------------------------------------------"""
-# mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
-# mlflow.set_experiment("MLflow Quickstart")
-
 adversarial_loss = BCELoss().cuda() if cuda else BCELoss()
 recon_loss = L1Loss().cuda() if cuda else L1Loss()
 
@@ -45,8 +37,8 @@ def l1_regularization(model, rate):
     return rate * l1_norm
 
 
-hyperparams_g = {'lr': 0.005, 'beta1': 0.86, 'beta2': 0.909}
-hyperparams_d = {'lr': 0.0005, 'beta1': 0.78, 'beta2': 0.952}
+hyperparams_g = {'lr': 0.003, 'beta1': 0.86, 'beta2': 0.909}
+hyperparams_d = {'lr': 0.0001, 'beta1': 0.78, 'beta2': 0.952}
 
 
 optimizer_G = torch.optim.Adam(
@@ -61,12 +53,33 @@ Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
 
 """-----------------------------------------------------data gen-----------------------------------------------------"""
-def sample_runs(n_row, z_dim):
-    # z = torch.normal(0, 1, (n_row ** 2, z_dim)).cuda() if cuda else torch.normal(0, 1, (n_row ** 2, z_dim))
-    z = AAE_archi.custom_dist((n_row ** 2, z_dim)).cuda() if cuda else AAE_archi.custom_dist((n_row ** 2, z_dim))
-    gen_input = decoder(z).cpu()
-    gen_data = gen_input.data.numpy()
-    np.savetxt('1.txt', gen_data)
+def interpolate(z1, z2, n_steps=10):
+    interpolations = []
+    for alpha in torch.linspace(0, 1, n_steps):
+        z = z1 * (1 - alpha) + z2 * alpha
+        interpolations.append(z)
+    return torch.stack(interpolations)
+
+def sample_runs():
+    # z = torch.normal(0, 1, (batches, z_dim)).cuda() if cuda else torch.normal(0, 1, (batches, z_dim))
+    # z = AAE_archi.custom_dist((batches, z_dim)).cuda() if cuda else AAE_archi.custom_dist((batches, z_dim))
+    # gen_input = decoder(z).cpu()
+    # gen_data = gen_input.data.numpy()
+    # np.savetxt('4.txt', gen_data)
+    with torch.no_grad():
+        n_interpolations = 4
+        n_samples_per_interpolation = 6046
+        z1 = torch.randn(n_interpolations, 112).cuda() if cuda else torch.randn(n_interpolations, 112)
+        z2 = torch.randn(n_interpolations, 112).cuda() if cuda else torch.randn(n_interpolations, 112)
+
+        samples = []
+        for i in range(n_interpolations):
+            interpolations = interpolate(z1[i], z2[i], n_samples_per_interpolation)
+            decoded_samples = decoder(interpolations).cuda() if cuda else decoder(interpolations)
+            samples.append(decoded_samples)
+
+        samples = torch.cat(samples, dim=0)
+        return samples
 
 
 """--------------------------------------------------model training--------------------------------------------------"""
@@ -74,15 +87,17 @@ for epoch in range(200):
     n_batch = len(df_train) // 64
     for i in range(n_batch):
         str_idx = i * 64
-        end_idx = str_idx+64
-        train_data_tensor = torch.tensor(df_train[str_idx:end_idx], dtype=torch.float).cuda() if cuda else torch.tensor(df_train[str_idx:end_idx], dtype=torch.float)
-        # real = (train_data_tensor - train_data_tensor.mean()) / train_data_tensor.std()
-        real = train_data_tensor
-        valid = torch.ones((train_data_tensor.shape[0], 1)).cuda() if cuda else torch.ones((train_data_tensor.shape[0], 1))
-        fake = torch.zeros((train_data_tensor.shape[0], 1)).cuda() if cuda else torch.zeros((train_data_tensor.shape[0], 1))
+        end_idx = str_idx+ 64
+        real = torch.tensor(df_train[str_idx:end_idx], dtype=torch.float).cuda() if cuda \
+            else torch.tensor(df_train[str_idx:end_idx], dtype=torch.float)
+        real = (real - real.mean()) / real.std()
+        valid = torch.ones((real.shape[0], 1)).cuda() if cuda else torch.ones((real.shape[0], 1))
+        fake = torch.zeros((real.shape[0], 1)).cuda() if cuda else torch.zeros((real.shape[0], 1))
+        noisy_real = real + torch.randn_like(real) * 0.1
 
         optimizer_G.zero_grad()
-        encoded = encoder_generator(real)
+        # encoded = encoder_generator(real)
+        encoded = encoder_generator(noisy_real)
         labels_tensor = AAE_archi.encoded_tensor[str_idx:end_idx]
         dec_input = torch.cat([encoded, labels_tensor], dim=1)
         decoded = decoder(dec_input)
@@ -93,10 +108,12 @@ for epoch in range(200):
         optimizer_G.step()
 
         optimizer_D.zero_grad()
-        z = torch.normal(0, 1, (train_data_tensor.shape[0], AAE_archi.z_dim)).cuda() if cuda else torch.normal(0, 1, (train_data_tensor.shape[0], AAE_archi.z_dim))
+        # real = (real - real.mean()) / real.std()
+        # encoded = encoder_generator(real)
+        z = torch.normal(0, 1, (real.shape[0], AAE_archi.z_dim)).cuda() if cuda else torch.normal(0, 1, (real.shape[0], AAE_archi.z_dim))
         real_loss = adversarial_loss(discriminator(z), valid)
         fake_loss = adversarial_loss(discriminator(encoded.detach()), fake)
-        l1_pen_d = l1_regularization(discriminator, 0.0001)
+        l1_pen_d = l1_regularization(discriminator, 0.00001)
         d_loss = 0.5 * (real_loss + fake_loss) + l1_pen_d
 
         d_loss.backward()
@@ -106,9 +123,10 @@ for epoch in range(200):
     # scheduler_D.step()
     print(epoch, d_loss.item(), g_loss.item())
 
-    batches_done = epoch * len(df_train)
-    if batches_done:
-        sample_runs(n_row=315, z_dim=140)
+samples = sample_runs()
+samples = samples.detach().cpu().numpy()
+np.savetxt('5.txt', samples)
+print("sample saved")
 
 """--------------------------------------------------model testing---------------------------------------------------"""
 n_splits = 5
@@ -128,14 +146,15 @@ for fold, (_, val_index) in enumerate(kf.split(df_train)):
         str_idx = i * 1
         end_idx = str_idx + 1
         with torch.no_grad():
-            val_tensor = torch.tensor(df_val[str_idx:end_idx], dtype=torch.float).cuda() if cuda else torch.tensor(df_val[str_idx:end_idx], dtype=torch.float)
-            # val_real = (val_tensor - val_tensor.mean()) / val_tensor.std()
-            val_real = val_tensor
+            val_real = torch.tensor(df_val[str_idx:end_idx], dtype=torch.float).cuda() if cuda else torch.tensor(df_val[str_idx:end_idx], dtype=torch.float)
+            val_real = (val_real - val_real.mean()) / val_real.std()
             val_encoded = encoder_generator(val_real)
             labels_tensor = AAE_archi.encoded_tensor[str_idx:end_idx]
             dec_input = torch.cat([val_encoded, labels_tensor], dim=1)
             val_decoded = decoder(dec_input)
         recon_loss_val = recon_loss(val_decoded, val_real)
+        # val_real = (val_real - val_real.mean()) / val_real.std()
+        # val_encoded = encoder_generator(val_real)
         valid_val = torch.ones((val_real.shape[0], 1)).cuda() if cuda else torch.ones((val_real.shape[0], 1))
         adv_loss_val = adversarial_loss(discriminator(val_encoded), valid_val)
         fold_recon_loss.append(recon_loss_val.item())
@@ -146,7 +165,5 @@ avg_recon_loss = np.mean(recon_losses)
 avg_adversarial_loss = np.mean(adversarial_losses)
 std_recon_loss = np.std(recon_losses)
 std_adversarial_loss = np.std(adversarial_losses)
-print(avg_recon_loss, std_recon_loss)
 print(avg_adversarial_loss, std_adversarial_loss)
-
-
+print(avg_recon_loss, std_recon_loss)
