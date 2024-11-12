f2a0ef0d91064dab7aa159dc649811dc37573325
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2024-11-12 11:44:06 +0100

    Update RL_main.py

--- DDPG/RL_main.py
+++ DDPG/RL_main.py
@@ -7,7 +7,7 @@ from AAE import AAE_archi_opt
 
 
 def main():
-    enc_gen = AAE_archi_opt.encoder_generator
+    enc_gen = AAE_archi_opt.EncoderGenerator(27, )
     """-----------------------------------------------Data Loader----------------------------------------------------"""
 
     train_loader = AAE_archi_opt.dataloader
@@ -17,10 +17,11 @@ def main():
     # test_loader = AAE_archi_opt.test_dl
 
     """----------------Model Settings-----------------------------------------------"""
-    d_model = AAE_archi_opt.discriminator
+    de_model = AAE_archi_opt.Decoder(12, )
+    d_model = AAE_archi_opt.Discriminator(12, )
     d_model.eval()
 
-    trainer = Trainer(train_loader, valid_loader, enc_gen, d_model)
+    trainer = Trainer(train_loader, valid_loader, enc_gen, d_model, de_model)
     trainer.train()
     # else:
     #     tester = Tester(test_loader, enc_gen, d_model)
