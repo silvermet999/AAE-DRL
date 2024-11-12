66c0f8249261bb0dc1b487bdb70f302b9b694288
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2024-11-12 11:45:00 +0100

    Update EnvClass.py

--- DDPG/EnvClass.py
+++ DDPG/EnvClass.py
@@ -4,17 +4,18 @@ import torch.nn as nn
 import os
 import numpy as np
 
+cuda=False
 
 class Env(nn.Module):
-    def __init__(self, model_G, model_D):
+    def __init__(self, model_G, model_D, model_de):
         super(Env, self).__init__()
 
         self._state = None
+        self.decoder = model_de
         self.generator = model_G
         self.disciminator = model_D
 
         self.d_reward_coeff =1
-        self.cl_reward_coeff =30
 
         # for calculating the discriminator reward
         self.hinge = torch.nn.HingeEmbeddingLoss()
@@ -33,15 +34,14 @@ class Env(nn.Module):
     def forward(self, action, episode_target, save_fig=False, t=None):
         # episodeTarget: the number that the RL agent is trying to find
         with (torch.no_grad()):
-            z = action.to("cuda") if torch.cuda.is_available() else action
-            gen_out = self.generator(z)
+            z = action.to("cuda") if cuda else action
+            decoded = self.decoder(z)
+            gen_out = self.generator(decoded)
             dis_judge = self.disciminator(gen_out)
 
         batch_size = len(episode_target)
         episode_target = episode_target.to(torch.int64)
-        # reward_cl = self.cl_reward_coeff * np.exp(gen_sample[1, episode_target].cpu().data.numpy())
-        reward_cl = - self.d_reward_coeff * self.hinge(dis_judge, -1 * torch.ones_like(dis_judge)).cpu().data.numpy()
-        reward = reward_cl
+        reward = - self.d_reward_coeff * self.hinge(dis_judge, -1 * torch.ones_like(dis_judge)).cpu().data.numpy()
 
         done = True
 
@@ -49,6 +49,6 @@ class Env(nn.Module):
         self.count += 1
 
         # the nextState
-        next_state = gen_out.detach()
-        self._state = gen_out
+        next_state = decoded.detach()
+        self._state = decoded
         return next_state, reward, done, dis_judge.cpu().data.numpy()
