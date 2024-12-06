3aacf2a228e92a14d14a2f6a364ae2ae3eedcd4b
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2024-12-06 22:37:03 +0100

    Update clf_optim.py

--- clfs/clf_optim.py
+++ clfs/clf_optim.py
@@ -1,32 +1,29 @@
 import numpy as np
 import optuna
+import pandas as pd
 
 import sklearn.ensemble
 import sklearn.model_selection
 import sklearn.svm
 import xgboost as xgb
-from imblearn.over_sampling import SMOTE
+from sklearn.neighbors import KNeighborsClassifier
 
-from AAE import main
+import classifier
 import sklearn.metrics
 
 
 
+
 def objective(trial):
-    X_train, y_train = SMOTE().fit_resample(main.X_train_rs, main.y_train)
+    X_train = pd.DataFrame(np.loadtxt('/AAE/Adam.txt'))
+    # X_train = pd.DataFrame(main_u.X_train_sc)
+    y_train = classifier.y
+
     # classifier_name = trial.suggest_categorical("classifier", ["SVC", "rf"])
     # if classifier_name == "SVC":
     #     svc_c = trial.suggest_float("svc_c", 1e-10, 1e10, log=True)
     #     classifier_obj = sklearn.svm.SVC(C=svc_c, gamma="auto")
 
-    # elif classifier_name == "adaboost":
-    #     ada_n_estimators = trial.suggest_int("ada_n_estimators", 50, 200)
-    #     ada_learning_rate = trial.suggest_float("ada_learning_rate", 1e-3, 1e-1, log=True)
-    #     classifier_obj = sklearn.ensemble.AdaBoostClassifier(
-    #         n_estimators=ada_n_estimators,
-    #         learning_rate=ada_learning_rate
-    #     )
-    #
     #
     # elif classifier_name == "gb":
     #     gb_n_estimators = trial.suggest_int("gb_n_estimators", 50, 200)
@@ -38,32 +35,41 @@ def objective(trial):
     #         max_depth=gb_max_depth
     #     )
 
-
-
-    rf_max_depth = trial.suggest_int("rf_max_depth", 10, 20)
-    rf_n_estimators = trial.suggest_int("rf_n_estimators", 50, 200)
-    classifier_obj = sklearn.ensemble.RandomForestClassifier(
-        max_depth=rf_max_depth,
-        n_estimators=rf_n_estimators,
+    k = trial.suggest_int('n_neighbors', 1, 26)
+    metric = trial.suggest_categorical('metric', ['euclidean', 'manhattan', 'minkowski'])
+    p = trial.suggest_int('p', 1, 3) if metric == 'minkowski' else 2
+    leaf_size = trial.suggest_int("leaf_size", 10, 80)
+    classifier_obj = KNeighborsClassifier(
+        n_neighbors=k, metric=metric, p=p, leaf_size=leaf_size
     )
+
+
+
+    # rf_max_depth = trial.suggest_int("rf_max_depth", 10, 20)
+    # rf_n_estimators = trial.suggest_int("rf_n_estimators", 50, 200)
+    # classifier_obj = sklearn.ensemble.RandomForestClassifier(
+    #     max_depth=rf_max_depth,
+    #     n_estimators=rf_n_estimators,
+    # )
 
     recall_scorer = sklearn.metrics.make_scorer(sklearn.metrics.recall_score, average='weighted')
     score = sklearn.model_selection.cross_val_score(classifier_obj, X_train, y_train, cv=5, scoring=recall_scorer).mean()
     return score
 
 def objectivexgb(trial):
-    X_train = main.X_train_rs
-    y_train = main.y_train
-    X_test = main.X_test_rs
-    y_test = main.y_test
-    X_train, y_train = SMOTE().fit_resample(X_train, y_train)
+
+    # X_train = main_u.X_train_sc
+    X_train = np.loadtxt('/home/silver/PycharmProjects/AAEDRL/AAE/best.txt')[:82716]
+    y_train = classifier.y_train
+    X_test = classifier.X_test_sc
+    y_test = classifier.y_test.to_numpy()
     dtrain = xgb.DMatrix(X_train, label=y_train)
     dvalid = xgb.DMatrix(X_test, label=y_test)
 
     param = {
         "verbosity": 0,
         "objective": "multi:softmax",
-        "num_class": 14,
+        "num_class": 26,
         # defines booster, gblinear for linear functions.
         "booster": trial.suggest_categorical("booster", ["gbtree", "gblinear", "dart"]),
         # L2 regularization weight.
@@ -78,7 +84,7 @@ def objectivexgb(trial):
 
     if param["booster"] in ["gbtree", "dart"]:
         # maximum depth of the tree, signifies complexity of the tree.
-        param["max_depth"] = trial.suggest_int("max_depth", 5, 50)
+        param["max_depth"] = trial.suggest_int("max_depth", 1, 50)
         # minimum child weight, larger the term more conservative the tree.
         param["min_child_weight"] = trial.suggest_int("min_child_weight", 2, 20)
         param["eta"] = trial.suggest_float("eta", 1e-8, 1.0, log=True)
@@ -99,7 +105,7 @@ def objectivexgb(trial):
     return recall
 
 study = optuna.create_study(direction="maximize")
-study.optimize(objective, n_trials=500)
+study.optimize(objectivexgb, n_trials=200)
 print(study.best_trial)
 print("Number of finished trials: ", len(study.trials))
 print("Best trial:")
@@ -113,5 +119,8 @@ for key, value in trial.params.items():
 best_params = study.best_params
 
 
-
-
+# [I 2024-12-06 22:22:56,284] Trial 85 finished with value: 0.9749032882011606 and parameters:
+# {'booster': 'gbtree', 'lambda': 1.955696877876371e-05, 'alpha': 3.4821998334693364e-13,
+# 'subsample': 0.9640929261558455, 'colsample_bytree': 0.9027725034365305, 'max_depth': 24,
+# 'min_child_weight': 7, 'eta': 1.4433639109132085e-08, 'gamma': 0.0009949289920304545,
+# 'grow_policy': 'depthwise'}. Best is trial 85 with value: 0.9749032882011606.
