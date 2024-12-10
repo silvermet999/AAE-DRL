6cf6785d0041524aee7493945c2a7e5eafb343f3
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2024-12-10 01:10:12 +0100

    Update main_u.py

--- data/main_u.py
+++ data/main_u.py
@@ -3,8 +3,9 @@ from collections import defaultdict
 from scipy import stats
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder, RobustScaler, MaxAbsScaler, StandardScaler, MinMaxScaler, \
-    PolynomialFeatures
+from sklearn.preprocessing import LabelEncoder, MaxAbsScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from fitter import Fitter, get_common_distributions
 import warnings
@@ -17,19 +18,30 @@ warnings.filterwarnings('ignore')
 """--------------------------------------------data exploration/cleaning--------------------------------------------"""
 train = pd.read_csv('/home/silver/UNSW_NB15_training-set.csv')
 test = pd.read_csv('/home/silver/UNSW_NB15_testing-set.csv')
+extra = pd.read_csv("/home/silver/PycharmProjects/AAEDRL/data/dataset.csv")
+extra = extra.rename(columns={
+    'sintpkt': 'sinpkt',
+    'dintpkt': 'dinpkt',
+    'ct_src_ ltm': "ct_src_ltm",
+    'Label': "label"
+})
 
-dfs = [train, test]
+dfs = [train, test, extra]
 df = pd.concat(dfs, ignore_index=True)
 df = df.drop(df.columns[df.nunique() == 1], axis = 1) # no change
 df = df.drop(df.columns[df.nunique() == len(df)], axis = 1) # no change
+df = df.drop(["id", "Unnamed: 0"], axis=1)
 
-
+df["rate"] = df["rate"].fillna(df["rate"].mean())
 df["proto"].replace("a/n", np.nan, inplace=True)
 df["service"].replace("-", np.nan, inplace=True)
 df["state"].replace("no", np.nan, inplace=True)
 
 df.fillna('Missing', inplace=True)
-
+df["attack_cat"] = df["attack_cat"].replace([' Fuzzers', ' Fuzzers '], "Fuzzers")
+df["attack_cat"] = df["attack_cat"].replace("Backdoors", "Backdoor")
+df["attack_cat"] = df["attack_cat"].replace(" Reconnaissance ", "Reconnaissance")
+df["attack_cat"] = df["attack_cat"].replace(" Shellcode ", "Shellcode")
 
 le = LabelEncoder()
 cols_le = ["attack_cat", "proto", "service", "state"]
@@ -46,6 +58,7 @@ for col in cols_le:
     value_counts = df[col].value_counts()
     singletons = value_counts[value_counts == 1].index
     df = df[~df[col].isin(singletons)] # 34549, 129 => deleted 12483 samples
+
 
 def cap_outliers_iqr(df, factor=40):
     capped_df = df.copy()
@@ -70,12 +83,32 @@ def corr(df):
             if corr_col != column:
                 df_corr = correlation.loc[corr_col, column]
                 f_corr[(column, corr_col)] = df_corr
-    f_corr = pd.DataFrame.from_dict(f_corr, orient="index")
-    f_corr = f_corr.drop_duplicates()
-    # f_corr.to_csv("f_corr.csv")
-    columns_to_drop = {col_pair[1] for col_pair in f_corr.index}
-    df = df.drop(columns=columns_to_drop)
-    return df
+
+    # plt.figure(figsize=(30, 20))
+    # sns.heatmap(correlation, annot=True, fmt=".2f", cmap="coolwarm", vmin=-1, vmax=1)
+    # plt.title("Correlation Matrix Heatmap")
+    # plt.savefig("corr.png")
+
+    # f_corr = pd.DataFrame.from_dict(f_corr, orient="index")
+    # f_corr = f_corr.drop_duplicates()
+    # columns_to_drop = {col_pair[0] for col_pair in f_corr.index}
+    # df = df.drop(columns=columns_to_drop)
+    return f_corr
+
+df = df.drop(['ct_srv_src', 'ct_srv_dst', 'ct_src_dport_ltm', 'ct_dst_src_ltm', 'ct_dst_ltm', 'ct_src_ltm',
+              'ct_ftp_cmd', 'synack', 'ackdat', 'dloss', 'sbytes', 'stcpb', 'sloss', "dloss", "dbytes", "sbytes",
+               'is_sm_ips_ports', 'dwin', 'swin', 'state', 'tcprtt', "is_ftp_login"
+               ], axis=1)
+
+generic = df[df['attack_cat'].isin([5])]
+genidx = generic.tail(184352).index
+df = df.drop(genidx)
+# malware_and_low_imp = df[df["attack_cat"].isin([0, 1, 8, 9, 7, 4])]
+# exploits = df[df["attack_cat"].isin([3, 2])]
+df["attack_cat"] = df["attack_cat"].replace([0, 1, 8, 9, 7, 4], 0)
+df["attack_cat"] = df["attack_cat"].replace([[3, 2]], 1)
+explidx = df[df['attack_cat'].isin([1])].tail(31756).index
+df = df.drop(explidx)
 
 
 """-----------------------------------------------vertical data split-----------------------------------------------"""
@@ -83,32 +116,34 @@ def vertical_split(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=45)
     return X_train, X_test, y_train, y_test
 
-# df = df[df["attack_cat"].isin([6, 5, 3, 4, 2, 7])]
 
-X = corr(df.drop(["attack_cat", "label"], axis = 1))
+X = df.drop(["attack_cat", "label"], axis = 1)
 y_bin = df["label"]
 
 
 X_train, X_test, y_train, y_test = vertical_split(X, y_bin)
-
-
-poly = PolynomialFeatures(2)
-# poly.fit_transform(X)
 
 def mac(df):
     scaler = MaxAbsScaler()
     df = scaler.fit_transform(df)
     return df
 
-X_train_disc = X_train[["proto", "service", "state", "is_ftp_login", "ct_flw_http_mthd", "ct_state_ttl"]]
-X_test_disc = X_test[["proto", "service", "state", "is_ftp_login", "ct_flw_http_mthd", "ct_state_ttl"]]
 
-X_train_cont = mac(X_train[[feature for feature in X_train.columns if feature not in X_train_disc]])
-X_test_cont = mac(X_test[[feature for feature in X_test.columns if feature not in X_test_disc]])
+def prep(X):
+    X_disc = X[["proto", "service", "ct_state_ttl", "ct_flw_http_mthd", "dttl"]]
+    X_cont = mac(X[[feature for feature in X.columns if feature not in X_disc]])
+    X_sc = np.concatenate((X_disc, X_cont), axis=1)
+    return X_sc
 
-X_train_sc = np.concatenate((X_train_disc, X_train_cont), axis=1)
-X_test_sc = np.concatenate((X_test_disc, X_test_cont), axis=1)
-y_train = y_train.to_numpy()
+
+X_train_sc = prep(X_train)
+X_test_sc = prep(X_test)
+
+def inverse_sc(X, synth):
+    max_abs_values = np.abs(X).max(axis=0)
+    synth_inv = synth * max_abs_values
+    return synth_inv
+
 #
 #
 # def kolmogorov_smirnov_test(column, dist='norm'):
@@ -128,7 +163,7 @@ y_train = y_train.to_numpy()
 #
 #     return best_fit
 #     return best_fit_distribution
-#
+
 # X_train_sc = pd.DataFrame(X_train_sc)
 # results = {}
 #
@@ -142,7 +177,7 @@ y_train = y_train.to_numpy()
 #     col_results.append(f"{best_fit}")
 #     results[idx] = col_results
 #
-# for idx in range(28):
+# for idx in range(22):
 #     if idx in results:
 #         print(f"{idx}")
 #         for result in results[idx]:
