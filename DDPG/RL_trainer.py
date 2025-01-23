c0fc7ca9d08a26bb04658e0655d520cf1c3ad88b
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2025-01-23 13:36:18 +0100

    Update RL_trainer.py

--- DDPG/RL_trainer.py
+++ DDPG/RL_trainer.py
@@ -1,7 +1,8 @@
 import torch
 import torch.utils.data
-import torch.nn.parallel
 import os
+
+import utils
 from EnvClass import Env
 
 import numpy as np
@@ -16,7 +17,7 @@ os.environ['TORCH_USE_CUDA_DSA'] = "1"
 
 cuda = True if torch.cuda.is_available() else False
 
-def evaluate_policy(policy, dataloader, env, episode_num=10, t=None):
+def evaluate_policy(policy, dataloader, env, episode_num=10):
     avg_reward = 0.
     env.reset()
 
@@ -24,11 +25,11 @@ def evaluate_policy(policy, dataloader, env, episode_num=10, t=None):
         input, label = dataloader.next_data()
         obs = env.set_state(input)
         done = False
-        episodeTarget = (label + torch.randint(4, label.shape))
+        episode_target = (label + torch.randint(4, label.shape)) % 4
+
         while not done:
-            action = policy.select_action(np.array(obs))
-            action = torch.tensor(action)
-            new_state, reward, done, _ = env(action, episodeTarget, t)
+            continuous_act, discrete_act = policy.select_action(obs)
+            new_state, reward, done = env(continuous_act, discrete_act, episode_target)
             avg_reward += reward
 
     avg_reward /= episode_num
@@ -36,8 +37,10 @@ def evaluate_policy(policy, dataloader, env, episode_num=10, t=None):
     return avg_reward
 
 
+
+
 class Trainer(object):
-    def __init__(self, train_loader, valid_loader, model_encoder, model_d, model_De, classifier, in_out):
+    def __init__(self, train_loader, valid_loader, model_encoder, model_d, model_De, classifier, in_out, discrete):
         np.random.seed(5)
         torch.manual_seed(5)
 
@@ -48,8 +51,7 @@ class Trainer(object):
         self.epoch_size = len(self.valid_loader)
         self.max_timesteps = 100000
 
-        self.batch_size = 2
-        self.batch_size_actor = 2
+        self.batch_size = 32
         self.eval_freq = 1000
         self.start_timesteps = 50
         self.max_episodes_steps = 1000000
@@ -62,15 +64,20 @@ class Trainer(object):
         self.classifier = classifier
 
         self.env = Env(self.encoder, self.D, self.De, self.classifier)
+        self.replay_buffer = utils.ReplayBuffer()
+
+
 
         self.state_dim = in_out
-        self.action_dim = 14
-        self.discrete_features = AAE_archi_opt.discrete
+        self.action_dim = 5
+        self.discrete_features = discrete
         self.max_action = 1
-
         self.policy = TD3(self.state_dim, self.action_dim, self.discrete_features, self.max_action)
 
+
+
         self.continue_timesteps = 0
+        self.save_models = True
 
         self.evaluations = []
 
@@ -89,20 +96,18 @@ class Trainer(object):
 
         for t in range(int(self.continue_timesteps), int(self.max_timesteps)):
             episode_timesteps += 1
-
             continuous_act, discrete_act = self.policy.select_action(state)
 
-            next_state, reward, done, _ = self.env(continuous_act, discrete_act, episode_target)
+            next_state, reward, done = self.env(continuous_act, discrete_act, episode_target)
 
             self.policy.store_transition(state, continuous_act, discrete_act,
-                          next_state, reward, done)
+                          next_state, reward, done, episode_target)
 
             state = next_state
             episode_reward += reward
 
             if t >= self.start_timesteps:
-                state, continuous_act, discrete_act, next_state, reward, done
-                self.policy.train(self.batch_size)
+                self.policy.train()
 
             if done:
                 state_t, label = self.train_loader.next_data()
@@ -112,7 +117,7 @@ class Trainer(object):
                 done = False
                 self.env.reset()
 
-                print('\rstep: {}, episode: {}, reward: {}'.format(t + 1, episode_num + 1, episode_reward), end='')
+                print('\repisode: {}, reward: {}'.format(episode_num + 1, episode_reward), end='')
                 sum_return += episode_reward
                 episode_reward = 0
                 episode_timesteps = 0
@@ -120,28 +125,41 @@ class Trainer(object):
 
             # Evaluate episode
             if (t + 1) % self.eval_freq == 0:
-                episode_result = "step: {} episode: {} average reward: {}".format(t + 1, episode_num,
+                episode_result = "episode: {} average reward: {}".format(episode_num,
                                                                                   sum_return / episode_num)
                 print('\r' + episode_result)
 
                 valid_episode_num = 6
                 self.evaluations.append(evaluate_policy(self.policy, self.valid_loader, self.env,
-                                                        episode_num=valid_episode_num, t=t))
-                eval_result = "evaluation over {} episodes: {}".format(valid_episode_num, self.evaluations[-1])
+                                                        episode_num=valid_episode_num))
+                eval_result = "episodes: {}".format(self.evaluations[-1])
                 print(eval_result)
 
+in_out = 30
+z_dim = 10
+label_dim = 4
+
+encoder_generator = AAE_archi_opt.EncoderGenerator(in_out, z_dim).cuda() if cuda else (
+    AAE_archi_opt.EncoderGenerator(in_out, z_dim))
+decoder = AAE_archi_opt.Decoder(z_dim, in_out, utils.discrete, utils.continuous, utils.binary)
+
+discrete = {"state": 5,
+            # "service": 13,
+            "ct_state_ttl": 6,
+            # "dttl": 9,
+            # "sttl": 13,
+            "trans_depth": 11,
+            "proto": 2,
+            "is_ftp_login":2
+}
 
 
-train_loader, val_loader = AAE_archi_opt.dataset_function(AAE_archi_opt.dataset, batch_size=2, train=True)
-encoder_generator = AAE_archi_opt.encoder_generator
-decoder = AAE_archi_opt.decoder
-
-
-discriminator = AAE_archi_opt.discriminator
+discriminator = AAE_archi_opt.Discriminator(z_dim, ).cuda() if cuda else (
+    AAE_archi_opt.Discriminator(z_dim, ))
 discriminator.eval()
 classifier = classifier.classifier
 classifier.eval()
 
+train_loader, val_loader = utils.dataset_function(utils.dataset, 32, 32, train=True)
 
-
-trainer = Trainer(train_loader, val_loader, encoder_generator, discriminator, decoder, classifier, 32)
+trainer = Trainer(train_loader, val_loader, encoder_generator, discriminator, decoder, classifier, in_out, discrete)
