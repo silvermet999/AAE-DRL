b1a1e4453699d7df1423c3e7ce3293e366db2e0d
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2025-01-23 13:33:11 +0100

    Update AAE_testing.py

--- AAE_testing.py
+++ AAE_testing.py
@@ -1,184 +1,103 @@
-"""-----------------------------------------------import libraries-----------------------------------------------"""
+import pandas as pd
+
+import utils
+from AAE import AAE_archi_opt
 import os
-from sklearn.model_selection import KFold
-from torch.optim.lr_scheduler import MultiStepLR
-
-from AAE import AAE_archi
-
 import numpy as np
 import torch
-from torch.nn import BCELoss, L1Loss
+from torch.nn.functional import binary_cross_entropy, one_hot
+
 from torch import cuda
-from data import main
 import itertools
-os.environ['CUDA_LAUNCH_BLOCKING']="1"
+from data import main_u
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 os.environ['TORCH_USE_CUDA_DSA'] = "1"
 
-
-
-cuda = True if cuda.is_available() else False
+cuda = True if torch.cuda.is_available() else False
 torch.cuda.empty_cache()
+torch.manual_seed(0)
 
 
-df_train = main.X_test_rs
+"""--------------------------------------------------dataset and models--------------------------------------------------"""
+in_out = 30
+z_dim = 10
+label_dim = 4
+
+# dataset = CustomDataset(main_u.X_train_sc.to_numpy(), main_u.y_train.to_numpy())
+df = pd.DataFrame(pd.read_csv("/home/silver/PycharmProjects/AAEDRL/AAE/ds.csv"))[:141649]
+X_train, X_test, y_train, y_test = main_u.vertical_split(df, main_u.y)
+dataset_synth = utils.CustomDataset(X_train.to_numpy(), y_train.to_numpy())
+test_loader =utils.dataset_function(dataset_synth, 32, 64, train=False)
 
 
-"""--------------------------------------------------loss and optim--------------------------------------------------"""
-# mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
-# mlflow.set_experiment("MLflow Quickstart")
+encoder_generator = AAE_archi_opt.EncoderGenerator(in_out, z_dim).cuda() if cuda else (
+    AAE_archi_opt.EncoderGenerator(in_out, z_dim))
 
-adversarial_loss = BCELoss().cuda() if cuda else BCELoss()
-recon_loss = L1Loss().cuda() if cuda else L1Loss()
+decoder = AAE_archi_opt.Decoder(z_dim+label_dim, in_out, utils.discrete, utils.continuous, utils.binary).cuda() if cuda \
+    else (AAE_archi_opt.Decoder(z_dim+label_dim, in_out, utils.discrete, utils.continuous, utils.binary))
 
+discriminator = AAE_archi_opt.Discriminator(z_dim, ).cuda() if cuda else (
+    AAE_archi_opt.Discriminator(z_dim, ))
 
-encoder_generator = AAE_archi.EncoderGenerator().cuda() if cuda else AAE_archi.EncoderGenerator()
-decoder = AAE_archi.Decoder().cuda() if cuda else AAE_archi.Decoder()
-discriminator = AAE_archi.Discriminator().cuda() if cuda else AAE_archi.Discriminator()
-
-
-
-hyperparams_g = {'lr': 0.001, 'beta1': 0.5, 'beta2': 0.999}
-hyperperams_d={'lrd':0.001, 'beta1d': 0.9, 'beta2d': 0.99} # stuck in local min
+optimizer_G = torch.optim.SGD(itertools.chain(encoder_generator.parameters(), decoder.parameters()),
+                              lr=0.000001)
+optimizer_D = torch.optim.SGD(discriminator.parameters(), lr=0.001)
 
 
-optimizer_G = torch.optim.Adam(
-    itertools.chain(encoder_generator.parameters(), decoder.parameters()), lr=hyperparams_g["lr"], betas=(hyperparams_g["beta1"], hyperparams_g["beta2"]))
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=hyperperams_d["lrd"], betas=(hyperperams_d["beta1d"], hyperperams_d["beta2d"]))
-scheduler_D = MultiStepLR(optimizer_D, milestones=[30, 80], gamma=0.01)
 
-scheduler_G = MultiStepLR(optimizer_G, milestones=[30, 80], gamma=0.01)
-
-Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+# Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
 
-"""--------------------------------------------------model training--------------------------------------------------"""
-for epoch in range(100):
-    n_batch = len(df_train) // 64
-    for i in range(n_batch):
-        str_idx = i * 64
-        end_idx = str_idx+64
-        train_data_tensor = torch.tensor(df_train[str_idx:end_idx], dtype=torch.float).cuda() if cuda else torch.tensor(df_train[str_idx:end_idx], dtype=torch.float)
-
-        real = (train_data_tensor - train_data_tensor.mean()) / train_data_tensor.std()
-        valid = torch.ones((train_data_tensor.shape[0], 1)).cuda() if cuda else torch.ones((train_data_tensor.shape[0], 1))
-        fake = torch.zeros((train_data_tensor.shape[0], 1)).cuda() if cuda else torch.zeros((train_data_tensor.shape[0], 1))
-
-        optimizer_G.zero_grad()
-        encoded = encoder_generator(real)
-        labels_tensor = AAE_archi.encoded_tensor[str_idx:end_idx]
-        dec_input = torch.cat([encoded, labels_tensor], dim=1)
-        decoded = decoder(dec_input)
-        g_loss = 0.001 * adversarial_loss(discriminator(encoded), valid) + 0.999 * recon_loss(decoded, real)
-
-        g_loss.backward()
-        optimizer_G.step()
-
-        optimizer_D.zero_grad()
-
-        # log_normal = torch.distributions.LogNormal(loc=0, scale=1)
-        # z = log_normal.sample((batch_data.shape[0], AAE_archi.z_dim)).cuda() if cuda else log_normal.sample((batch_data.shape[0], AAE_archi.z_dim))
-        z = torch.normal(0, 1, (train_data_tensor.shape[0], AAE_archi.z_dim)).cuda() if cuda else torch.normal(0, 1, (train_data_tensor.shape[0], AAE_archi.z_dim))
-
-        # real and fake loss should be close
-        # discriminator(z) should be close to 0e
-        real_loss = adversarial_loss(discriminator(z), valid)
-        fake_loss = adversarial_loss(discriminator(encoded.detach()), fake)
-        d_loss = 0.5 * (real_loss + fake_loss)
-
-        d_loss.backward()
-        optimizer_D.step()
-
-    # scheduler_G.step()
-    # scheduler_D.step()
-    print(epoch, d_loss.item(), g_loss.item())
-
-    """-------------------------------------------------save model---------------------------------------------------"""
-#     if (epoch + 1) % 20 == 0:
-#         torch.save(encoder_generator.state_dict(), f'encoder_generator_epoch_{epoch + 1}.pth')
-#         torch.save(decoder.state_dict(), f'decoder_epoch_{epoch + 1}.pth')
-#         torch.save(discriminator.state_dict(), f'discriminator_epoch_{epoch + 1}.pth')
-#
-# torch.save(encoder_generator.state_dict(), 'encoder_generator_final.pth')
-# torch.save(decoder.state_dict(), 'decoder_final.pth')
-# torch.save(discriminator.state_dict(), 'discriminator_final.pth')
-#
-# encoder_generator.load_state_dict(torch.load('encoder_generator_final.pth'))
-# decoder.load_state_dict(torch.load('decoder_final.pth'))
-# discriminator.load_state_dict(torch.load('discriminator_final.pth'))
-
-avg_g_loss = np.mean(g_loss.item())
-std_g_loss = np.std(g_loss.item())
-
-
-"""--------------------------------------------------model testing---------------------------------------------------"""
-n_splits = 5
-kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
-recon_losses = []
-adversarial_losses = []
-for fold, (_, val_index) in enumerate(kf.split(df_train)):
-    print(f"Fold {fold + 1}/{n_splits}")
-    df_val = df_train[val_index]
-    fold_recon_loss = []
-    fold_adversarial_loss = []
+def test_model(test_loader):
     encoder_generator.eval()
     decoder.eval()
     discriminator.eval()
-    n_batch = len(df_val)
-    for i in range(n_batch):
-        str_idx = i * 1
-        end_idx = str_idx + 1
-        with torch.no_grad():
-            val_tensor = torch.tensor(df_val[str_idx:end_idx], dtype=torch.float).cuda() if cuda else torch.tensor(df_val[str_idx:end_idx], dtype=torch.float)
-            val_real = (val_tensor - val_tensor.mean()) / val_tensor.std()
-            val_encoded = encoder_generator(val_real)
-            labels_tensor = AAE_archi.encoded_tensor[str_idx:end_idx]
-            dec_input = torch.cat([val_encoded, labels_tensor], dim=1)
-            val_decoded = decoder(dec_input)
-        recon_loss_val = recon_loss(val_decoded, val_real)
-        valid_val = torch.ones((val_real.shape[0], 1)).cuda() if cuda else torch.ones((val_real.shape[0], 1))
-        adv_loss_val = adversarial_loss(discriminator(val_encoded), valid_val)
-        fold_recon_loss.append(recon_loss_val.item())
-        fold_adversarial_loss.append(adv_loss_val.item())
-    recon_losses.append(np.mean(fold_recon_loss))
-    adversarial_losses.append(np.mean(fold_adversarial_loss))
-avg_recon_loss = np.mean(recon_losses)
-avg_adversarial_loss = np.mean(adversarial_losses)
-std_recon_loss = np.std(recon_losses)
-std_adversarial_loss = np.std(adversarial_losses)
-print(f"Average Reconstruction Loss: {avg_recon_loss:.4f} ± {std_recon_loss:.4f}")
-print(f"Average Adversarial Loss: {avg_adversarial_loss:.4f} ± {std_adversarial_loss:.4f}")
 
+    total_g_loss = 0
+    total_d_loss = 0
 
-"""--------------------------------------------------mlflow---------------------------------------------------"""
+    with torch.no_grad():
+        for X, y in test_loader:
+            valid = torch.ones((X.shape[0], 1), requires_grad=False).cuda() if cuda else torch.ones((X.shape[0], 1),
+                                                                                                    requires_grad=False)
+            fake = torch.zeros((X.shape[0], 1), requires_grad=False).cuda() if cuda else torch.zeros((X.shape[0], 1),
+                                                                                                     requires_grad=False)
+            real = X.type(torch.FloatTensor).cuda() if cuda else X.type(torch.FloatTensor)
+            y = y.type(torch.LongTensor).cuda() if cuda else y.type(torch.LongTensor)
+            y = one_hot(y, num_classes=4)
 
-# with mlflow.start_run():
-#     mlflow.set_tag("Training Info", "Test")
-#
-#     mlflow.log_metric("test_avg_recon_loss", avg_recon_loss)
-#     mlflow.log_metric("test_std_recon_loss", std_recon_loss)
-#     mlflow.log_metric("test_avg_adversarial_loss", avg_adversarial_loss)
-#     mlflow.log_metric("test_std_adversarial_loss", std_adversarial_loss)
-#
-#     # Log individual fold results
-#     for fold, (recon_loss, adv_loss) in enumerate(zip(recon_losses, adversarial_losses)):
-#         mlflow.log_metric(f"test_fold_{fold + 1}_recon_loss", recon_loss)
-#         mlflow.log_metric(f"test_fold_{fold + 1}_adv_loss", adv_loss)
-#
-#     mlflow.set_tag("Model Info", "Adversarial Autoencoder")
-#     mlflow.set_tag("Evaluation", "5-Fold Cross-Validation")
-#
-#     model_info_gen = mlflow.sklearn.log_model(
-#         sk_model = encoder_generator,
-#         artifact_path="mlflow/gen",
-#         input_example=AAE_archi.in_out,
-#         registered_model_name="supervised_G_tracking",
-#     )
-#     model_info_disc = mlflow.sklearn.log_model(
-#         sk_model=discriminator,
-#         artifact_path="mlflow/discriminator",
-#         input_example=AAE_archi.z_dim,
-#         registered_model_name="supervisedD_tracking",
-#     )
-# Average Reconstruction Loss: 0.4099 ± 0.0106
-# Average Adversarial Loss: 0.7027 ± 0.0202
+            discrete_targets = {}
+            continuous_targets = {}
+            binary_targets = {}
+            for feature, _ in decoder.discrete_features.items():
+                discrete_targets[feature] = torch.ones(real.shape[0])
 
+            for feature in decoder.continuous_features:
+                continuous_targets[feature] = torch.ones(real.shape[0])
+
+            for feature in decoder.binary_features:
+                binary_targets[feature] = torch.ones(real.shape[0])
+
+            encoded = encoder_generator(real)
+            dec_input = torch.cat([encoded, y], dim=1)
+            discrete_outputs, continuous_outputs, binary_outputs = decoder(dec_input)
+
+            g_loss = (0.1 * binary_cross_entropy(discriminator(encoded), valid) +
+                      0.9 * decoder.compute_loss((discrete_outputs, continuous_outputs, binary_outputs),
+                                                 (discrete_targets, continuous_targets, binary_targets)))
+
+            z = utils.custom_dist((real.shape[0], z_dim)).cuda() if cuda else utils.custom_dist((real.shape[0], z_dim))
+            real_loss = binary_cross_entropy(discriminator(z), valid)
+            fake_loss = binary_cross_entropy(discriminator(encoded.detach()), fake)
+            d_loss = 0.5 * (real_loss + fake_loss)
+
+            total_g_loss += g_loss.item()
+            total_d_loss += d_loss.item()
+
+    avg_g_loss = total_g_loss / len(test_loader)
+    avg_d_loss = total_d_loss / len(test_loader)
+
+    return {
+        'g_loss': avg_g_loss,
+        'd_loss': avg_d_loss
+    }
