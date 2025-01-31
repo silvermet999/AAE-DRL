5778d526355045dfc19eb9b6153d157a2576d946
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2025-02-01 00:36:42 +0100

    Update EnvClass.py

--- DDPG/EnvClass.py
+++ DDPG/EnvClass.py
@@ -1,16 +1,18 @@
 # libraries
 import torch
-import torch.nn as nn
+from torch.nn import Module, BCELoss, CrossEntropyLoss
 import os
+
+from torch.nn.functional import one_hot
+
 import utils
 
-from AAE import AAE_archi_opt
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 os.environ['TORCH_USE_CUDA_DSA'] = "1"
 cuda = True if torch.cuda.is_available() else False
 
-class Env(nn.Module):
+class Env(Module):
     def __init__(self, model_G, model_D, model_de, classifier):
         super(Env, self).__init__()
 
@@ -20,10 +22,10 @@ class Env(nn.Module):
         self.disciminator = model_D
         self.classifier = classifier
 
-        self.d_reward_coeff = 1
-        self.cl_reward_coeff = 0.5
-        self.bin = torch.nn.BCELoss().cuda() if cuda else torch.nn.BCELoss()
-        self.ce = torch.nn.CrossEntropyLoss().cuda() if cuda else torch.nn.CrossEntropyLoss()
+        self.d_reward_coeff = 0.3
+        self.cl_reward_coeff = 0.1
+        self.bin = BCELoss().cuda() if cuda else BCELoss()
+        self.ce = CrossEntropyLoss().cuda() if cuda else CrossEntropyLoss()
 
         self.count = 0
 
@@ -39,33 +41,31 @@ class Env(nn.Module):
         c_decoded = {feature: [] for feature in self.decoder.continuous_features}
         b_decoded = {feature: [] for feature in self.decoder.binary_features}
         with (torch.no_grad()):
+            episode_target = episode_target.to(torch.long).cuda() if cuda else episode_target.to(torch.long)
+            labels = one_hot(episode_target, num_classes=4)
             z_cont = torch.tensor(action).cuda() if cuda else torch.tensor(action)
             z_disc = torch.tensor(list(disc.values())).cuda() if cuda else torch.tensor(list(disc.values()))
-            z_disc = z_disc.expand(z_cont.size(0), -1)
-            z = torch.concat([z_cont, z_disc], 1)
+            z_disc = z_disc.expand(z_cont.size(0), -1).cuda() if cuda else z_disc.expand(z_cont.size(0), -1)
+            labels = labels.squeeze(1)
+            z = torch.concat([z_cont, z_disc, labels], 1)
             d, c, b = self.decoder(z)
-            d_decoded, c_decoded, b_decoded = AAE_archi_opt.types_append(d, c, b, d_decoded, c_decoded, b_decoded)
-            d_decoded, c_decoded, b_decoded = AAE_archi_opt.type_concat(d_decoded, c_decoded, b_decoded)
+            d_decoded, c_decoded, b_decoded = utils.types_append(self.decoder, d, c, b, d_decoded, c_decoded, b_decoded)
+            d_decoded, c_decoded, b_decoded = utils.type_concat(self.decoder, d_decoded, c_decoded, b_decoded)
             decoded = utils.all_samples(d_decoded, c_decoded, b_decoded)
             gen_out = self.generator(decoded)
             dis_judge = self.disciminator(gen_out)
             classifier_output, _ = self.classifier.encoder(decoded)
             classifier_logits, _ = self.classifier.classify(classifier_output)
 
-        episode_target = episode_target.to(torch.long).cuda() if cuda else episode_target.to(torch.long)
         if len(episode_target.shape) == 2:
             episode_target = episode_target.squeeze(1)
-        reward_cl = self.cl_reward_coeff * abs(self.ce(classifier_logits, episode_target).cpu().data.numpy())
+        reward_cl = self.cl_reward_coeff * self.ce(classifier_logits, episode_target).cpu().data.numpy()
         reward_d = self.d_reward_coeff * self.bin(dis_judge, torch.ones_like(dis_judge)).cpu().data.numpy()
 
         reward = reward_cl + reward_d
-
         done = True
-
-
         self.count += 1
 
-        # the nextState
         next_state = decoded.detach().cpu().data.numpy()
         self._state = decoded
         return next_state, reward, done
