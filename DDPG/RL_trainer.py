6782963f6124ae9160d01c8775f5e1ed9eb59abf
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2025-02-01 00:35:47 +0100

    Update RL_trainer.py

--- DDPG/RL_trainer.py
+++ DDPG/RL_trainer.py
@@ -1,12 +1,19 @@
+import csv
+import random
+
+import pandas as pd
 import torch
 import torch.utils.data
 import os
+
+from torch.nn.functional import one_hot
 
 import utils
 from EnvClass import Env
 
 import numpy as np
 
+from data import main_u
 from utils import RL_dataloader
 from RL import TD3
 from AAE import AAE_archi_opt
@@ -17,13 +24,16 @@ os.environ['TORCH_USE_CUDA_DSA'] = "1"
 
 cuda = True if torch.cuda.is_available() else False
 
-def evaluate_policy(policy, dataloader, env, episode_num=10):
+def evaluate_policy(policy, dataloader, env, eval_episodes=10):
+    torch.manual_seed(0)
+    np.random.seed(0)
     avg_reward = 0.
-    env.reset()
 
-    for i in range(0, episode_num):
+    # for i in range(0, episode_num):
+    for _ in range(eval_episodes):
         input, label = dataloader.next_data()
         obs = env.set_state(input)
+        env.reset()
         done = False
         episode_target = (label + torch.randint(4, label.shape)) % 4
 
@@ -32,7 +42,7 @@ def evaluate_policy(policy, dataloader, env, episode_num=10):
             new_state, reward, done = env(continuous_act, discrete_act, episode_target)
             avg_reward += reward
 
-    avg_reward /= episode_num
+    avg_reward /= eval_episodes
 
     return avg_reward
 
@@ -40,41 +50,36 @@ def evaluate_policy(policy, dataloader, env, episode_num=10):
 
 
 class Trainer(object):
-    def __init__(self, train_loader, valid_loader, model_encoder, model_d, model_De, classifier, in_out, discrete):
-        np.random.seed(5)
-        torch.manual_seed(5)
-
-
+    def __init__(self, train_loader, valid_loader, model_encoder, model_disc, model_decoder, classifier, in_out, discrete):
         self.train_loader = RL_dataloader(train_loader)
         self.valid_loader = RL_dataloader(valid_loader)
 
         self.epoch_size = len(self.valid_loader)
-        self.max_timesteps = 100000
+        self.max_timesteps = 10000
 
         self.batch_size = 32
-        self.eval_freq = 1000
+        self.eval_freq = 500
         self.start_timesteps = 50
-        self.max_episodes_steps = 1000000
+        self.max_episodes_steps = 10000
 
-        self.expl_noise = 0
+        self.expl_noise = 0.3
 
         self.encoder = model_encoder
-        self.D = model_d
-        self.De = model_De
+        self.discriminator = model_disc
+        self.decoder = model_decoder
         self.classifier = classifier
 
-        self.env = Env(self.encoder, self.D, self.De, self.classifier)
+        self.env = Env(self.encoder, self.discriminator, self.decoder, self.classifier)
         self.replay_buffer = utils.ReplayBuffer()
 
-
+        torch.manual_seed(0)
+        np.random.seed(0)
 
         self.state_dim = in_out
-        self.action_dim = 5
+        self.action_dim = 7
         self.discrete_features = discrete
         self.max_action = 1
         self.policy = TD3(self.state_dim, self.action_dim, self.discrete_features, self.max_action)
-
-
 
         self.continue_timesteps = 0
         self.save_models = True
@@ -86,6 +91,9 @@ class Trainer(object):
         episode_reward = 0
         episode_timesteps = 0
         episode_num = 0
+        d_list = []
+        c_list = []
+        b_list = []
 
         state_t, label = self.train_loader.next_data()
         state = self.env.set_state(state_t)
@@ -96,18 +104,24 @@ class Trainer(object):
 
         for t in range(int(self.continue_timesteps), int(self.max_timesteps)):
             episode_timesteps += 1
-            continuous_act, discrete_act = self.policy.select_action(state)
+            if t < self.start_timesteps:
+                continuous_act = torch.randn(self.batch_size, self.action_dim)
+                discrete_act = {name: random.randrange(num_actions) for name, num_actions in self.discrete_features.items()}
+            else:
+                continuous_act, discrete_act = self.policy.select_action(state)
+                continuous_act = torch.tensor(continuous_act + np.random.normal(0, self.max_action * self.expl_noise,
+                                                        size=self.action_dim)).clip(-self.max_action, self.max_action).float()
 
             next_state, reward, done = self.env(continuous_act, discrete_act, episode_target)
 
-            self.policy.store_transition(state, continuous_act, discrete_act,
-                          next_state, reward, done, episode_target)
+            self.policy.store_transition(state, continuous_act, discrete_act, next_state, reward, done, episode_target)
 
             state = next_state
             episode_reward += reward
 
             if t >= self.start_timesteps:
                 self.policy.train()
+
 
             if done:
                 state_t, label = self.train_loader.next_data()
@@ -129,37 +143,91 @@ class Trainer(object):
                                                                                   sum_return / episode_num)
                 print('\r' + episode_result)
 
-                valid_episode_num = 6
-                self.evaluations.append(evaluate_policy(self.policy, self.valid_loader, self.env,
-                                                        episode_num=valid_episode_num))
+                self.evaluations.append(evaluate_policy(self.policy, self.valid_loader, self.env))
                 eval_result = "episodes: {}".format(self.evaluations[-1])
                 print(eval_result)
+
+            new_state = self.encoder(torch.tensor(state).float().cuda() if cuda else torch.tensor(state).float())
+            labels = one_hot(episode_target, num_classes=4)
+            nd, nc, nb = self.decoder(torch.cat([new_state, labels.squeeze(1)], dim=1))
+            d_list.append(nd)
+            c_list.append(nc)
+            b_list.append(nb)
+        d_cat = {key: torch.cat([d[key] for d in d_list], dim=0) for key in d_list[0]}
+        c_cat = {key: torch.cat([d[key] for d in c_list], dim=0) for key in c_list[0]}
+        b_cat = {key: torch.cat([d[key] for d in b_list], dim=0) for key in b_list[0]}
+        return d_cat, c_cat, b_cat
+
+
+
 
 in_out = 30
 z_dim = 10
 label_dim = 4
 
-encoder_generator = AAE_archi_opt.EncoderGenerator(in_out, z_dim).cuda() if cuda else (
-    AAE_archi_opt.EncoderGenerator(in_out, z_dim))
-decoder = AAE_archi_opt.Decoder(z_dim, in_out, utils.discrete, utils.continuous, utils.binary)
-
-discrete = {"state": 5,
-            # "service": 13,
-            "ct_state_ttl": 6,
-            # "dttl": 9,
-            # "sttl": 13,
+discrete = {"ct_state_ttl": 6,
             "trans_depth": 11,
             "proto": 2,
-            "is_ftp_login":2
 }
-
-
+encoder_generator = AAE_archi_opt.EncoderGenerator(in_out, z_dim).cuda() if cuda else (
+    AAE_archi_opt.EncoderGenerator(in_out, z_dim))
+encoder_generator.load_state_dict(torch.load("/home/silver/PycharmProjects/AAEDRL/AAE/enc_gen.pth", map_location=torch.device('cpu')))
+decoder = AAE_archi_opt.Decoder(z_dim+label_dim, in_out, utils.discrete, utils.continuous, utils.binary).cuda() if cuda else (
+    AAE_archi_opt.Decoder(z_dim+label_dim, in_out, utils.discrete, utils.continuous, utils.binary))
+decoder.load_state_dict(torch.load("/home/silver/PycharmProjects/AAEDRL/AAE/dec.pth", map_location=torch.device('cpu')))
 discriminator = AAE_archi_opt.Discriminator(z_dim, ).cuda() if cuda else (
     AAE_archi_opt.Discriminator(z_dim, ))
+discriminator.load_state_dict(torch.load("/home/silver/PycharmProjects/AAEDRL/AAE/disc.pth", map_location=torch.device('cpu')))
 discriminator.eval()
 classifier = classifier.classifier
 classifier.eval()
 
-train_loader, val_loader = utils.dataset_function(utils.dataset, 32, 32, train=True)
 
-trainer = Trainer(train_loader, val_loader, encoder_generator, discriminator, decoder, classifier, in_out, discrete)
+dataset = utils.dataset(original=False)
+train_loader, val_loader = utils.dataset_function(dataset, 32, 32, train=True)
+
+d, c, b = Trainer(train_loader, val_loader, encoder_generator, discriminator, decoder, classifier, in_out, discrete).train()
+
+d_dict = {key: tensor.detach().cpu().numpy() for key, tensor in d.items()}
+c_dict = {key: tensor.detach().cpu().numpy() for key, tensor in c.items()}
+b_dict = {key: tensor.detach().cpu().numpy() for key, tensor in b.items()}
+
+d_max = {key: np.argmax(value, axis=1) for key, value in d_dict.items()}
+b_max = {key: np.argmax(value, axis=1) for key, value in b_dict.items()}
+
+# disc = pd.DataFrame(pd.read_csv("/home/silver/PycharmProjects/AAEDRL/DDPG/disc.csv"))
+# cont = pd.DataFrame(pd.read_csv("/home/silver/PycharmProjects/AAEDRL/DDPG/cont.csv"))
+# cont = cont.apply(lambda col: col.str.strip("[]").astype(float))
+# bin = pd.DataFrame(pd.read_csv("/home/silver/PycharmProjects/AAEDRL/DDPG/bin.csv"))
+# X = pd.concat([disc, cont, bin], axis=1)
+# X.to_csv("rl_ds.csv", index=False)
+
+
+# with open('disc.csv', 'w', newline='') as file_d:
+#     writer = csv.writer(file_d)
+#     keys = list(d_decoded.keys())
+#     writer.writerow(keys)
+#     max_len = max(len(d_decoded[key]) for key in keys)
+#     for i in range(max_len):
+#         row = [d_decoded[key][i] if i < len(d_decoded[key]) else '' for key in keys]
+#         writer.writerow(row)
+#
+#
+#
+# with open('cont.csv', 'w', newline='') as file_c:
+#     writer = csv.writer(file_c)
+#     keys = list(c_decoded.keys())
+#     writer.writerow(keys)
+#     max_len = max(len(c_decoded[key]) for key in keys)
+#     for i in range(max_len):
+#         row = [c_decoded[key][i] if i < len(c_decoded[key]) else '' for key in keys]
+#         writer.writerow(row)
+#
+# with open('bin.csv', 'w', newline='') as file_b:
+#     writer = csv.writer(file_b)
+#     keys = list(b_decoded.keys())
+#     writer.writerow(keys)
+#     max_len = max(len(b_decoded[key]) for key in keys)
+#     for i in range(max_len):
+#         row = [b_decoded[key][i] if i < len(b_decoded[key]) else '' for key in keys]
+#         writer.writerow(row)
