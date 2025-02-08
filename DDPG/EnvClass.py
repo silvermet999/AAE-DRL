5aa647fe8ca4ec27fb94bb18a65041a9e04ea6a9
silvermet999 <122080119+silvermet999@users.noreply.github.com>
2025-02-08 12:08:31 +0100

    Update EnvClass.py

--- DDPG/EnvClass.py
+++ DDPG/EnvClass.py
@@ -22,15 +22,16 @@ class Env(Module):
         self.disciminator = model_D
         self.classifier = classifier
 
-        self.d_reward_coeff = 0.3
-        self.cl_reward_coeff = 0.1
-        self.bin = BCELoss().cuda() if cuda else BCELoss()
-        self.ce = CrossEntropyLoss().cuda() if cuda else CrossEntropyLoss()
+        self.d_reward_coeff = 0.4
+        self.cl_reward_coeff = 0.3
 
         self.count = 0
 
     def reset(self):
+        self._state = None
         self.count = 0
+        return self._state
+
 
     def set_state(self, state):
         self._state = state
@@ -40,29 +41,46 @@ class Env(Module):
         d_decoded = {feature: [] for feature in self.decoder.discrete_features}
         c_decoded = {feature: [] for feature in self.decoder.continuous_features}
         b_decoded = {feature: [] for feature in self.decoder.binary_features}
+
         with (torch.no_grad()):
             episode_target = episode_target.to(torch.long).cuda() if cuda else episode_target.to(torch.long)
             labels = one_hot(episode_target, num_classes=4)
+            episode_target_indices = labels.argmax(dim=1)
             z_cont = torch.tensor(action).cuda() if cuda else torch.tensor(action)
             z_disc = torch.tensor(list(disc.values())).cuda() if cuda else torch.tensor(list(disc.values()))
             z_disc = z_disc.expand(z_cont.size(0), -1).cuda() if cuda else z_disc.expand(z_cont.size(0), -1)
             labels = labels.squeeze(1)
-            z = torch.concat([z_cont, z_disc, labels], 1)
-            d, c, b = self.decoder(z)
-            d_decoded, c_decoded, b_decoded = utils.types_append(self.decoder, d, c, b, d_decoded, c_decoded, b_decoded)
+            for i in range(32):
+                z = torch.concat([z_cont[i].unsqueeze(0), z_disc[i].unsqueeze(0), labels[i].unsqueeze(0)], 1)
+                d, c, b = self.decoder(z)
+                d_decoded, c_decoded, b_decoded = utils.types_append(self.decoder, d, c, b, d_decoded, c_decoded, b_decoded)
             d_decoded, c_decoded, b_decoded = utils.type_concat(self.decoder, d_decoded, c_decoded, b_decoded)
             decoded = utils.all_samples(d_decoded, c_decoded, b_decoded)
             gen_out = self.generator(decoded)
             dis_judge = self.disciminator(gen_out)
-            classifier_output, _ = self.classifier.encoder(decoded)
-            classifier_logits, _ = self.classifier.classify(classifier_output)
+            print(f"Input shape: {decoded.shape}")
+            max_abs_val = torch.max(torch.abs(decoded), dim=0)[0]
+            decoded_scaled = decoded / (max_abs_val + 1e-7)
+            classifier_output, entropy = self.classifier.encoder(decoded_scaled)
+            print(f"Encoder output range: [{classifier_output.min():.3f}, {classifier_output.max():.3f}]")
+            print(f"Encoder output shape: {classifier_output.shape}")
+            print(f"Entropy: {entropy:.3f}")
+            classifier_logits, predictions = self.classifier.classify(classifier_output)
+            print(f"Logits range: [{classifier_logits.min():.3f}, {classifier_logits.max():.3f}]")
+            print(f"Predictions distribution:")
+            print(predictions.sum(dim=0))
 
-        if len(episode_target.shape) == 2:
-            episode_target = episode_target.squeeze(1)
-        reward_cl = self.cl_reward_coeff * self.ce(classifier_logits, episode_target).cpu().data.numpy()
-        reward_d = self.d_reward_coeff * self.bin(dis_judge, torch.ones_like(dis_judge)).cpu().data.numpy()
 
-        reward = reward_cl + reward_d
+            reward_cl = self.cl_reward_coeff * torch.nn.functional.cross_entropy(classifier_logits, episode_target_indices).cpu().data.numpy()
+            reward_d = self.d_reward_coeff * torch.nn.functional.binary_cross_entropy(dis_judge, torch.ones((dis_judge.shape[0], 1),
+                                                                                      requires_grad=False).cuda()
+            if cuda else torch.ones((dis_judge.shape[0], 1),requires_grad=False)).cpu().data.numpy()
+
+            print("d", torch.nn.functional.binary_cross_entropy(dis_judge, torch.ones((dis_judge.shape[0], 1),requires_grad=False).cuda() if cuda else
+            torch.ones((dis_judge.shape[0], 1),requires_grad=False)).cpu().data.numpy())
+            print("cl", torch.nn.functional.cross_entropy(classifier_logits, labels.float()).cpu().data.numpy())
+
+            reward = reward_cl + reward_d
         done = True
         self.count += 1
 
